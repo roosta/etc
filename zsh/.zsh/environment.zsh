370ae5a82ec5c15926290c1222c4eb057e73b549
Daniel Berg <roosta042@gmail.com>
2015-06-20 14:50:06 +0200

    Update shell env vars, rm upstream gtkfilechooser

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -20,3 +20,5 @@ export LESS_TERMCAP_md=$(printf '\e[1;34m')
 export LESS_TERMCAP_us=$(printf '\e[1;32m')
 export LESS_TERMCAP_so=$(printf '\e[1;44;1m')
 
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
