1174754fd9b667324ec99dbb36c0b581fede8aa2
Daniel Berg <roosta042@gmail.com>
2015-07-02 07:57:57 +0200

    Update i3 conky wrapper and rc

--- i3/.i3/i3-conky-wrapper.sh
+++ i3/.i3/i3-conky-wrapper.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-echo "{\"version\":1}"
-echo "[[] ,"
-conky -c ~/.i3/i3-conky-status.old
-#conky -c ~/.i3/i3-conky-status
+
+# Send the header so that i3bar knows we want to use JSON:
+echo '{"version":1}'
+
+# Begin the endless array.
+echo '['
+
+# We send an empty first array of blocks to make the loop simpler:
+echo '[],'
+
+# Now send blocks with information forever:
+exec conky -c $HOME/.i3/i3-conky-status.old
