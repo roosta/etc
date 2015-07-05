b40a6a30f659107b38f642b0ff019c42b14eef68
Daniel Berg <roosta042@gmail.com>
2015-07-05 22:55:36 +0200

    Update i3 colors, font config. Add zsh conf. Rm fonts
    Update i3 colors, and add various zsh configs. Some im currently just
    trying out (syntax, and a new completion setup)
    
    fonts are now maintained in a spideroak folder, since keeping it here
    got too annoying. Also seeing I bought some fonts keeping them a public repo
    isn't such a good idea :)

--- zsh/.zsh/autocompletion.zsh
+++ zsh/.zsh/autocompletion.zsh
@@ -1,21 +1,21 @@
 
-autoload -Uz compinit
-compinit
-# autoload -U ~/.zsh/completion/*(:t)
+#autoload -Uz compinit
+#compinit
+## autoload -U ~/.zsh/completion/*(:t)
 
-zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
-zstyle ':completion:*' format 'Completing %d'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' insert-unambiguous true
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' 'r:|[._-]=** r:|=**' 'l:|=* r:|=*'
-zstyle ':completion:*' menu select=5
-zstyle ':completion:*' original true
-zstyle ':completion:*' preserve-prefix '//[^/]##/'
-zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-zstyle ':completion:*' squeeze-slashes true
-zstyle ':completion:*' verbose true
-zstyle ':completion:*' insert-tab pending
-# zstyle :compinstall filename '/home/daniel/.zshrc'
+#zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
+#zstyle ':completion:*' format 'Completing %d'
+#zstyle ':completion:*' group-name ''
+#zstyle ':completion:*' insert-unambiguous true
+#zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+#zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+#zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' 'r:|[._-]=** r:|=**' 'l:|=* r:|=*'
+#zstyle ':completion:*' menu select=5
+#zstyle ':completion:*' original true
+#zstyle ':completion:*' preserve-prefix '//[^/]##/'
+#zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+#zstyle ':completion:*' squeeze-slashes true
+#zstyle ':completion:*' verbose true
+#zstyle ':completion:*' insert-tab pending
+## zstyle :compinstall filename '/home/daniel/.zshrc'
 
