9b5d4223b05a2605c7a47499493348a291b593f3
Daniel Berg <mail@roosta.sh>
2015-11-01 10:48:52 +0100

    Large update to zsh completion.
    Cleanup and fix much of the completion to match my taste.
    @@ This file was for a long time mainly copy pasted from two sources,
       and I haven't looked at it since. Sources are zsh-lovers and
       oh-my-zsh. Added some personal tastes and tried to get rid of
       much unwanted settings.

--- zsh/.zsh/completion.zsh
+++ zsh/.zsh/completion.zsh
@@ -3,161 +3,127 @@
 # │░░░█░░░█░█░█░█░█▀▀░█░░░█▀▀░░█░░░█░░█░█░█░█░░░░│
 # │░░░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░░▀░░▀▀▀░▀▀▀░▀░▀░░░░│
 # └──────────────────────────────────────────────┘
+# thanks to:
+# - https://github.com/xero/dotfiles/blob/master/zsh/.zsh/autocompletion.zsh
+# - https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/completion.zsh
+# - http://dotshare.it/dots/100/
 
+# init
 autoload -Uz compinit
 compinit
 autoload -U ~/.zsh/completion/*(:t)
 
-# autoload -U ~/.zsh/completion/*(:t)
-zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
-zstyle ':completion:*' format 'Completing %d'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' insert-unambiguous true
-#zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-#zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-#zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' 'r:|[._-]=** r:|=**' 'l:|=* r:|=*'
-#zstyle ':completion:*' menu select=5
-zstyle ':completion:*' original true
-zstyle ':completion:*' preserve-prefix '//[^/]##/'
-#zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-#zstyle ':completion:*' squeeze-slashes true
-#zstyle ':completion:*' verbose true
-zstyle ':completion:*' insert-tab pending
+# set some options first
+unsetopt menu_complete      # do not autoselect the first completion entry
+unsetopt flowcontrol
+setopt   auto_menu          # show completion menu on succesive tab press
+setopt   complete_in_word
+setopt   always_to_end
 
-# rehash automatically
-zstyle ':completion:*' rehash true
+# general
+zstyle ':completion:*'         rehash true
+zstyle ':completion:*'         auto-description 'specify: %d'
+zstyle ':completion:*'         completer _expand _complete _correct _approximate
+zstyle ':completion:*'         format 'Completing %d'
+zstyle ':completion:*'         group-name ''
 
-# source: https://github.com/xero/dotfiles/blob/master/zsh/.zsh/autocompletion.zsh
-#zstyle ':completion:*' auto-description 'specify: %d'
-#zstyle ':completion:*' completer _expand _complete _correct _approximate
-#zstyle ':completion:*' format 'Completing %d'
-#zstyle ':completion:*' group-name ''
-#zstyle ':completion:*' menu select=2 eval "$(dircolors -b)"
-#zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-#zstyle ':completion:*' list-colors ''
-#zstyle ':completion:*' list-prompt %SAt %p: hit TAB for more, or the character to insert%s
-#zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
-#zstyle ':completion:*' menu select=long
-#zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-#zstyle ':completion:*' use-compctl false
-#zstyle ':completion:*' verbose true
-#zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
-#zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+# use dircolors for menus
+zstyle ':completion:*'         menu select=2 eval "$(dircolors -b)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
-# source:  http://dotshare.it/dots/100/
+zstyle ':completion:*'         list-colors ''
+zstyle ':completion:*'         list-prompt %SAt %p: hit TAB for more, or the character to insert%s
+
+# If you end up using a directory as argument, this will remove the trailing slash (usefull in ln)
+zstyle ':completion:*'         squeeze-slashes true
+
+zstyle ':completion:*'         matcher-list '' 'm:{[:lower:]}={[:upper:]}' 'r:|[._-]=** r:|=**' 'l:|=* r:|=*'
+#zstyle ':completion:*'         matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*'         menu select=long #200
+zstyle ':completion:*'         select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*'         use-compctl false
+zstyle ':completion:*'         verbose true
+zstyle ':completion:*'         accept-exact '*(N)'
+zstyle ':completion:*'         separate-sections 'yes'
+zstyle ':completion:*'         list-dirs-first true
+zstyle ':completion:*'         use-perl=1
+
+# formatting
+zstyle ':completion:*:descriptions' format $'%{- \e[38;5;137;1m\e[48;5;234m%}%B%d%b%{\e[m%}'
+zstyle ':completion:*:warnings'     format $'%{No match for \e[38;5;240;1m%}%d%{\e[m%}'
+
 ## ignore completion to commands we don't have
-zstyle ':completion:*:functions'          ignored-patterns '_*'
-
-## format autocompletion style
-zstyle ':completion:*:descriptions'       format "%{$c1%}%d%{$reset_color%}"
-zstyle ':completion:*:corrections'        format "%{$c3%}%d%{$reset_color%}"
-zstyle ':completion:*:messages'           format "%{$c1%}%d%{$reset_color%}"
-zstyle ':completion:*:warnings'           format "%{$c1%}%d%{$reset_color%}"
-
-## zstyle show completion menu if 2 or more items to select
-zstyle ':completion:*'                    menu select=2
+zstyle ':completion:*:functions' ignored-patterns '_*'
 
 ## zstyle kill menu
 zstyle ':completion:*:*:kill:*'           menu yes select
 zstyle ':completion:*:kill:*'             force-list always
+zstyle ':completion:*:kill:*'             command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
 zstyle ':completion:*:*:kill:*:processes' list-colors "=(#b) #([0-9]#)*=36=31"
+#zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+#zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 
-zstyle ':completion:*'                       accept-exact '*(N)'
-zstyle ':completion:*'                       separate-sections 'yes'
-zstyle ':completion:*'                       list-dirs-first true
-zstyle ':completion:*:default'               list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*'                       menu select=200
-zstyle ':completion:*'                       use-perl=1
+# completion cache
+zstyle ':completion::complete:*'       use-cache 1
+zstyle ':completion::complete:*'       cache-path $ZSH_CACHE_DIR
 
-zstyle ':completion:*'                       squeeze-slashes true
-##zstyle ':completion:*:cd:*'                  ignore-parents parent pwd
-##zstyle ':completion:*:cd:*'                  tag-order 'named-directories'
 
-zstyle ':completion:*:(all-|)files'          ignored-patterns '*.un~'
-zstyle ':completion:*:*:kill:*:processes' \
-  list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:processes'       command 'ps -axw'
+zstyle ':completion:*:processes-names' command 'ps -awxho command'
+zstyle ':completion:*:functions'       ignored-patterns '_*'
 
-zstyle ':completion::complete:*'             use-cache on
-zstyle ':completion::complete:*'             cache-path ~/etc/cache/$HOST
-zstyle ':completion:*:processes'             command 'ps -axw'
-zstyle ':completion:*:processes-names'       command 'ps -awxho command'
-zstyle ':completion:*'                       matcher-list 'm:{a-z}={A-Z}'
-zstyle ':completion:*:functions'             ignored-patterns '_*'
-
-#zstyle ':completion:*' group-name            ''
+# mplayer
 zstyle ':completion:*:*:mplayer:*'           tag-order files
 zstyle ':completion:*:*:mplayer:*'           file-patterns   \
        '*.(rmvb|mkv|mpg|wmv|mpeg|avi|flv|mp3|mp4|flac|ogg):video' \
        '*:all-files' '*(-/):directories'
 
-zstyle ':completion:*:*:(vim|rview|vimdiff|xxd):*:*files' \
-  ignored-patterns '*~|*.(old|bak|zwc|viminfo|rxvt-*|zcompdump)|pm_to_blib|cover_db|blib' \
-  file-sort modification
-zstyle ':completion:*:*:(vim|rview|vimdiff|xxd):*' \
-  file-sort modification
-zstyle ':completion:*:*:(vim|rview|vimdiff|xxd):*' \
-  tag-order files
-##zstyle ':completion:*:vim:*:directories' ignored-patterns \*
+# vim
+zstyle ':completion:*:*:(vim|rview|vimdiff|xxd):*'        file-sort modification
+zstyle ':completion:*:*:(vim|rview|vimdiff|xxd):*'        tag-order files
+zstyle ':completion:*:vim:*:directories'                  ignored-patterns \*
+zstyle ':completion:*:*:(vim|rview|vimdiff|xxd):*:*files' ignored-patterns \
+       '*~|*.(old|bak|zwc|viminfo|rxvt-*|zcompdump)|pm_to_blib|cover_db|blib' file-sort modification
 
-zstyle ':completion:*:*:(scp):*' \
-  file-sort modification
-
-zstyle ':completion:*:*:(cd):*:*files' ignored-patterns '*~' file-sort access
+# change directory
+#zstyle ':completion:*:*:(cd):*:*files' ignored-patterns '*~' file-sort access
 zstyle ':completion:*:*:(cd):*'        file-sort access
 zstyle ':completion:*:*:(cd):*'        menu select
 zstyle ':completion:*:*:(cd):*'        completer _history
 
+# cd will never select the parent directory (e.g.: cd ../<TAB>):
+zstyle ':completion:*:cd:*' ignore-parents parent pwd
+
 zstyle ':completion:*:*:perl:*'        file-patterns '*'
 
+# Method for quick change directories.
+rationalise-dot() {
+  if [[ $LBUFFER = *.. ]]; then
+    LBUFFER+=/..
+  else
+    LBUFFER+=.
+  fi
+}
+zle -N rationalise-dot
+bindkey . rationalise-dot
 
-zstyle ':completion:*:descriptions' \
-  format $'%{- \e[38;5;137;1m\e[48;5;234m%}%B%d%b%{\e[m%}'
-zstyle ':completion:*:warnings' \
-  format $'%{No match for \e[38;5;240;1m%}%d%{\e[m%}'
-
-zstyle ':completion:*:*:apvlv:*'             tag-order files
-zstyle ':completion:*:*:apvlv:*'             file-patterns '*.pdf'
-
+# MRU
 zstyle ':completion:most-accessed-file:*' match-original both
 zstyle ':completion:most-accessed-file:*' file-sort access
 zstyle ':completion:most-accessed-file:*' file-patterns '*:all\ files'
 zstyle ':completion:most-accessed-file:*' hidden all
 zstyle ':completion:most-accessed-file:*' completer _files
 
-
-zstyle ':completion:*:scp:*' group-order \
-      users files all-files hosts-domain hosts-host hosts-ipaddr
-
-zstyle ':completion:*:ssh:*' tag-order \
-      users 'hosts:-host hosts:-domain:domain hosts:-ipaddr:IP\ address *'
-
-zstyle ':completion:*:ssh:*' group-order \
-      hosts-domain hosts-host users hosts-ipaddr
-
-zstyle ':completion:*:(ssh|scp):*:hosts-host' ignored-patterns \
-      '*.*' loopback localhost
-
-zstyle ':completion:*:(ssh|scp):*:hosts-domain' ignored-patterns \
-      '<->.<->.<->.<->' '^*.*' '*@*'
-
-zstyle ':completion:*:(ssh|scp):*:hosts-ipaddr' ignored-patterns \
-      '^<->.<->.<->.<->' '127.0.0.<->'
-
-zstyle ':completion:*:(ssh|scp):*:users' ignored-patterns \
-      adm bin daemon halt lp named shutdown sync
-
-#zstyle ':completion:*:(ssh|scp):*:my-accounts' users-hosts \
-  #'crshd@shellmix.com' 'mobile@192.168.0.112' 'mobile@192.168.1.49'
-
-
+# Don't complete uninteresting users
 zstyle ':completion:*:*:*:users' ignored-patterns \
-        adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
-        dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
-        hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
-        mailman mailnull mldonkey mysql nagios \
-        named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
-        operator pcap postfix postgres privoxy pulse pvm quagga radvd \
-        rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs
-zstyle '*' single-ignored show
+        adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+        clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+        gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+        ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
+        named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+        operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+        rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+        usbmux uucp vcsa wwwrun xfs '_*'
 
+# ... unless we really want to.
+zstyle '*' single-ignored show
