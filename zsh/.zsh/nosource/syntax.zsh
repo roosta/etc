6162f82fbea326d23cc395e613fd5d4d468e05a9
Daniel Berg <mail@roosta.sh>
2015-11-14 11:39:43 +0100

    Add source order to certain zsh configurations.
    * Add a new folder to contain *.zsh that is excluded from being sourced
      on shell init.
    
    ``Needed to add an order to spesifically prompt and keybind configs to
      make sure one creates a function, and the other one appends to it.

---
+++ zsh/.zsh/nosource/syntax.zsh
@@ -0,0 +1,62 @@
+# ┌───────────────────────────────────────────────┐
+# │░█▀▀░█░█░█▀█░▀█▀░█▀█░█░█░░░█▀▀░█░█░▀█▀░█▀▄░█▀█░│
+# │░▀▀█░░█░░█░█░░█░░█▀█░▄▀▄░░░█▀▀░▄▀▄░░█░░█▀▄░█▀█░│
+# │░▀▀▀░░▀░░▀░▀░░▀░░▀░▀░▀░▀░░░▀▀▀░▀░▀░░▀░░▀░▀░▀░▀░│
+# └───────────────────────────────────────────────┘
+# add some extra syntax highlighting
+# source: http://dotshare.it/dots/100/
+#         https://github.com/xero/dotfiles/blob/master/zsh/.zsh/syntax.zsh
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+
+ STYLES
+# Aliases and functions
+ZSH_HIGHLIGHT_STYLES[alias]='fg=blue,bold'
+ZSH_HIGHLIGHT_STYLES[function]='fg=blue,bold'
+
+ Commands and builtins
+ZSH_HIGHLIGHT_STYLES[command]="fg=cyan,bold"
+ZSH_HIGHLIGHT_STYLES[hashed-command]="fg=cyan,bold"
+ZSH_HIGHLIGHT_STYLES[builtin]="fg=cyan,bold"
+
+ Paths
+ZSH_HIGHLIGHT_STYLES[path]='fg=white'
+
+ Globbing
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=yellow,bold'
+
+# Options and arguments
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=magenta'
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=magenta'
+
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]="fg=green"
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]="fg=green"
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]="fg=green"
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]="fg=green"
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]="fg=green"
+
+ZSH_HIGHLIGHT_STYLES[default]='none'
+ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red'
+ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=yellow'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[precommand]='none'
+ZSH_HIGHLIGHT_STYLES[commandseparator]='none'
+ZSH_HIGHLIGHT_STYLES[path_prefix]='none'
+ZSH_HIGHLIGHT_STYLES[path_approx]='fg=yellow'
+ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=yellow'
+ZSH_HIGHLIGHT_STYLES[assign]='none'
+
+# PATTERNS
+# mark dangerous commands with red
+ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=black,bold,bg=red')
+ZSH_HIGHLIGHT_PATTERNS+=('rmrf *' 'fg=black,bold,bg=red')
+
+# Sudo
+ZSH_HIGHLIGHT_PATTERNS+=('sudo ' 'fg=black,bold,bg=red')
+
+# colors for permissions
+if [[ "$EUID" -ne "0" ]]
+then  # if user is not root
+    USER_LEVEL="%F{cyan}"
+else # root!
+    USER_LEVEL="%F{red}"
+fi
