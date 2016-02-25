2be434d9ddd5f718834210fca7e067c577a96e38
Daniel Berg <mail@roosta.sh>
2016-02-25 10:03:47 +0100

    Update profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,14 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
+
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
+
         :plugins [[cljfmt "0.3.0"]
                   [cider/cider-nrepl "0.10.2"]
                   [venantius/ultra "0.4.0"]
                   [lein-ancient "0.6.8"]
                   [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]]
+
+        :repl {:plugins [[cider/cider-nrepl "0.10.2"]]}
+
         :ultra {:color-scheme :solarized_dark}}}
