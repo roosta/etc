58cd0743817b26a6798166ffe4516976560c6e40
Daniel Berg <mail@roosta.sh>
2016-03-04 19:22:15 +0100

    Add kibit linter to user profile (lein)

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -7,7 +7,8 @@
                   [venantius/ultra "0.4.0"]
                   [lein-ancient "0.6.8"]
                   [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-                  [lein-pprint "1.1.1"]]
+                  [lein-pprint "1.1.1"]
+                  [lein-kibit "0.1.2"]]
         :repl-options {:init (require 'cljfmt.core)}
         :ultra {:color-scheme :solarized_dark}
         :injections [(require 'pjstadig.humane-test-output)
