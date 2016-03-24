6a5174b88205024a9409de28d98a5c78224daa60
Daniel Berg <mail@roosta.sh>
2016-03-24 21:40:09 +0100

    Minor zsh tweaks

--- zsh/.zsh/env.zsh
+++ zsh/.zsh/env.zsh
@@ -65,4 +65,4 @@ PERL_MB_OPT="--install_base \"${HOME}/perl5\""; export PERL_MB_OPT;
 PERL_MM_OPT="INSTALL_BASE=${HOME}/perl5"; export PERL_MM_OPT;
 
 # set site.USERBASE for pip installs
-#export PYTHONUSERBASE=/usr/local/lib/pip
+export PYTHONUSERBASE=$HOME/.pip
