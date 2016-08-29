2b2038810e6d0ae285e6aa7e2fe9203e588e39b5
Daniel Berg <mail@roosta.sh>
2016-08-29 22:52:40 +0200

    Update deps.

--- conf/lein/.lein/profiles.clj
+++ conf/lein/.lein/profiles.clj
@@ -1,19 +1,18 @@
-{:user {:dependencies [[pjstadig/humane-test-output "0.8.0"]
+{:user {:dependencies [[pjstadig/humane-test-output "0.8.1"]
                        [alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]
-                       ]
+                       [org.clojure/tools.nrepl "0.2.12"]]
         :plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
                   [venantius/ultra "0.4.1"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
                   [lein-ancient "0.6.10"]
                   [jonase/eastwood "0.2.3"]
-                  [lein-pprint "1.1.1"]
+                  [lein-pprint "1.1.2"]
                   [lein-kibit "0.1.2"]]
         :ultra {:color-scheme :solarized_dark}
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}
 
  :android-common {:android {:sdk-path "/home/roosta/lib/android-sdk-linux"}}
- :android-user {:dependencies [[cider/cider-nrepl "0.9.1"]]
+ :android-user {:dependencies [[cider/cider-nrepl "0.14.0-SNAPSHOT"]]
                 :android {:aot-exclude-ns ["cider.nrepl.middleware.util.java.parser"
                                            "cider.nrepl" "cider-nrepl.plugin"]}}}
