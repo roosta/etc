fb62e806334cf6839f4af4174ba8d29620986576
Daniel Berg <mail@roosta.sh>
2016-10-03 08:52:05 +0200

    Move some files out of functions folder.

--- conf/zsh/.zsh.d/functions/md.zsh
+++ conf/zsh/.zsh.d/functions/md.zsh
@@ -1,6 +1,6 @@
 # read markdown files like manpages
 # https://github.com/xero/dotfiles/blob/master/zsh/.zsh/aliases.zsh
-md () {
+function md() {
     pandoc -s -f markdown -t man "$*" | man -l -
 }
 
