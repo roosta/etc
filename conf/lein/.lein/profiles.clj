b0c1db9df5b4b07e9427b87f6ac70dab0c4888ea
Daniel Berg <mail@roosta.sh>
2016-10-03 08:53:46 +0200

    Move some plugins to repl profile.

--- conf/lein/.lein/profiles.clj
+++ conf/lein/.lein/profiles.clj
@@ -1,17 +1,19 @@
 {:user {:dependencies [[pjstadig/humane-test-output "0.8.1"]
                        [alembic "0.3.2"]
-                       [org.clojure/clojure "1.8.0"]
-                       [org.clojure/tools.nrepl "0.2.12"]]
-        :plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
-                  [venantius/ultra "0.4.1"]
-                  [refactor-nrepl "2.3.0-SNAPSHOT"]
-                  [lein-ancient "0.6.10"]
+                       [org.clojure/clojure "1.8.0"]]
+        :plugins [[lein-ancient "0.6.10"]
                   [jonase/eastwood "0.2.3"]
                   [lein-pprint "1.1.2"]
                   [lein-kibit "0.1.2"]]
-        :ultra {:color-scheme :solarized_dark}
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}
+
+ :repl {:plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
+                  [venantius/ultra "0.4.1"]
+                  [refactor-nrepl "2.3.0-SNAPSHOT"]]
+        :ultra {:color-scheme :solarized_dark}
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}
 
  :android-common {:android {:sdk-path "/home/roosta/lib/android-sdk-linux"}}
  :android-user {:dependencies [[cider/cider-nrepl "0.14.0-SNAPSHOT"]]
