ab6a5a9c122f5ba60472d140802ed687edeb68ef
Daniel Berg <mail@roosta.sh>
2017-01-19 12:30:20 +0100

    Update profile deps

--- conf/lein/.lein/profiles.clj
+++ conf/lein/.lein/profiles.clj
@@ -3,22 +3,25 @@
                        ; [alembic "0.3.2"]
                        [com.cemerick/piggieback "0.2.1"]
                        [org.clojure/clojurescript "1.9.293"]
+                       [org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/clojure "1.9.0-alpha14"] ]
 
         :plugins [[lein-ancient "0.6.10"]
                   [jonase/eastwood "0.2.3"]
                   [lein-pprint "1.1.2"]
-                  [lein-kibit "0.1.3"]]
+                  [lein-kibit "0.1.3"]
+                  ]
         ; :injections [(require 'pjstadig.humane-test-output)
         ;              (pjstadig.humane-test-output/activate!)]
         }
  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
  :repl {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                  [venantius/ultra "0.5.0"]
+                  [venantius/ultra "0.5.1"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]]
         :ultra {:color-scheme :solarized_dark}
         :dependencies [[alembic "0.3.2"]
+                       ; [acyclic/squiggly-clojure "0.1.7"]
                        [org.clojure/tools.nrepl "0.2.12"]]}
 
  :android-common {:android {:sdk-path "/home/roosta/lib/android-sdk-linux"}}
