67fff73cec69cb50d22d77b420ec4aa5ac311406
Daniel Berg <mail@roosta.sh>
2017-01-29 18:50:34 +0100

    Simplify xrandr screenlayout script @ lintilla

--- local/lintilla/conf/screenlayout/.screenlayout/main.sh
+++ local/lintilla/conf/screenlayout/.screenlayout/main.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-       --output HDMI-0 --mode 1920x1080 --pos 5760x0 --rotate normal \
-       --output DP-1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --dpi 192 \
-       --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off \
+xrandr --output DVI-D-0 --auto --left-of DP-1 \
+       --output DP-1 --primary --auto --dpi 192 \
+       --output HDMI-0 --auto --right-of DP-1 
