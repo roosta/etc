ba77160c1f67e63e165b8e8a5ce3ea889014915a
Daniel Berg <mail@roosta.sh>
2017-02-07 13:57:23 +0100

    Update screen layout @ allitnil

--- local/allitnil/conf/screenlayout/.screenlayout/main.sh
+++ local/allitnil/conf/screenlayout/.screenlayout/main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI2 --off --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off --dpi 120
+xrandr --output eDP1 --primary --mode 1920x1080 --dpi 140
