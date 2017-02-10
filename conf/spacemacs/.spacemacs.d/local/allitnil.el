ace2c30174ae9c54df8082414a6cc68f91aa372c
Daniel Berg <mail@roosta.sh>
2017-02-10 15:39:42 +0100

    Add local conf

---
+++ conf/spacemacs/.spacemacs.d/local/allitnil.el
@@ -0,0 +1,9 @@
+(setq-default 
+  dotspacemacs-default-font '("Essential PragmataPro"
+                              :size 18
+                              :weight normal
+                              :width normal
+                              :powerline-scale 1.1))
+
+; dotspacemacs-default-font (load-file (concat "~/.spacemacs.d/local/" system-name ".el"))
+; :size (if (string= system-name "allitnil") 15 13)
