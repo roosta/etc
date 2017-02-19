4760f72f01d806ee67edbbb40067b799f6f1fcc3
Daniel Berg <mail@roosta.sh>
2017-02-19 01:30:22 +0100

    Update conky conf

---
+++ conf/conky/.config/conky/vpn.lua
@@ -0,0 +1,14 @@
+local handle = io.popen("pgrep -a openvpn")
+local result = handle:read("*a")
+handle:close()
+local match = string.match(result, "Air.-%d%d%d")
+
+function conky_vpn()
+  if match ~= nil then
+    return string.format("${color 98BC37}%s", match)
+  else
+    return "${color FF3128}No VPN detected"
+  end
+end
+
+print(conky_vpn())
