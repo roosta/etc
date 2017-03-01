0f1b15028c1e6aefff6929c2af002f25df150da0
Daniel Berg <mail@roosta.sh>
2017-03-01 20:45:07 +0100

    Fix vpn script

--- conf/conky/.config/conky/vpn.lua
+++ conf/conky/.config/conky/vpn.lua
@@ -1,9 +1,9 @@
-local handle = io.popen("pgrep -a openvpn")
-local result = handle:read("*a")
-handle:close()
-local match = string.match(result, "Air.-%d%d%d")
-
 function conky_vpn()
+  local handle = io.popen("pgrep -a openvpn")
+  local result = handle:read("*a")
+  handle:close()
+  local match = string.match(result, "Air.-%d%d%d")
+
   if match ~= nil then
     return string.format("${color 98BC37}%s", match)
   else
