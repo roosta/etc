1cb1fc1d94e7043ce5fce0116e005c58635c2e33
Daniel Berg <mail@roosta.sh>
2017-03-14 19:57:34 +0100

    WIP: Initial attempt at right side

--- conf/polybar/.config/polybar/pkg.sh
+++ conf/polybar/.config/polybar/pkg.sh
@@ -2,11 +2,12 @@
 pac=$(checkupdates | wc -l)
 aur=$(cower -u | wc -l)
 
-# check=$((pac + aur))
-# if [[ "$check" != "0" ]]
-# then
-#     echo "$pac %{F#5b5b5b}%{F-} $aur"
-# fi
+check=$((pac + aur))
+if [[ "$check" != "0" ]]; then
+  echo "%{B#FBB829 F#2D2B28}%{F-} $pac  $aur %{B#FBB829}"
+else
+  echo "%{B#2D2B28 F#FCE8C3} $pac  $aur "
+fi
 
 # echo "$pac % $aur"
-echo "$pac  $aur"
+# echo "$pac  $aur"
