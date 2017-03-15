d245f000e0ad3db4a0e2aeac51e05a3fc1272267
Daniel Berg <mail@roosta.sh>
2017-03-15 09:27:48 +0100

    Fix vpn formatting

--- conf/polybar/.config/polybar/pkg.sh
+++ conf/polybar/.config/polybar/pkg.sh
@@ -4,7 +4,7 @@ aur=$(cower -u | wc -l)
 
 check=$((pac + aur))
 if [[ "$check" != "0" ]]; then
-  echo "%{F#5573A3 B#00000000 T3}%{F- B- T-}%{F#FCE8C3 B#5573A3} $pac  $aur %{F- B-}%{F#5573A3 B#00000000 T3} %{F- B- T-}"
+  echo "%{F#5573A3 B#00000000 T3}%{F- B- T-}%{F#FCE8C3 B#5573A3} $pac  $aur %{F- B-}%{F#5573A3 B#2D2B28 T3} %{F- B- T-}"
 else
   echo "%{B#2D2B28 F#FCE8C3} $pac  $aur "
 fi
---
+++ conf/polybar/.config/polybar/vpn.sh
@@ -0,0 +1,6 @@
+vpn=$(pgrep -x openvpn)
+if [[ -n $vpn ]]; then
+  echo "%{B#00000000 F#FCE8C3 T3}%{B- F- T-}%{F#FCE8C3 B#2D2B28} VPN  Con %{F- B-}%{F#2D2B28 B#00000000 T3}%{F- B- T-"
+else
+  echo "%{B#F75341 F#2D2B28}%{F-} VPN  Dis "
+fi
