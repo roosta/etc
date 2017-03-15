c3884b753b0faf2d51438d3a07d8e9eaba6a8ba2
Daniel Berg <mail@roosta.sh>
2017-03-15 21:15:53 +0100

    Finishish vpn module formatting

--- conf/polybar/.config/polybar/pkg.sh
+++ conf/polybar/.config/polybar/pkg.sh
@@ -6,7 +6,8 @@ check=$((pac + aur))
 if [[ "$check" != "0" ]]; then
   echo "%{F#5573A3 B#00000000 T3}%{F- B- T-}%{F#FCE8C3 B#5573A3} $pac  $aur %{F- B-}%{F#5573A3 B#2D2B28 T3} %{F- B- T-}"
 else
-  echo "%{B#2D2B28 F#FCE8C3} $pac  $aur "
+  echo "%{F#FCE8C3 B#2D2B28} $pac  $aur %{F#2D2B28 B#00000000 T3} %{F- B- T-}"
+  # echo "%{B#2D2B28 F#FCE8C3} $pac  $aur "
 fi
 
 # echo "$pac % $aur"
