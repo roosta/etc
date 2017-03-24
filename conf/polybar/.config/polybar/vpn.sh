d8a385f97839a90a6853fcd57b3c1ee3776e5d87
Daniel Berg <mail@roosta.sh>
2017-03-24 20:18:30 +0100

    Improve color selection

--- conf/polybar/.config/polybar/vpn.sh
+++ conf/polybar/.config/polybar/vpn.sh
@@ -1,6 +1,6 @@
 vpn=$(pgrep -x openvpn)
 if [[ -n $vpn ]]; then
-  echo "%{F#2D2B28 B#00000000 T4}%{B- F- T-}%{F#FCE8C3 B#2D2B28} VPN  Con %{F- B-}%{F#2D2B28 B#00000000 T4}%{F- B- T-}"
+  echo "%{F#2D2B28 B#00000000 T4}%{B- F- T-}%{F#519F50 B#2D2B28} VPN %{F- B-}%{F#2D2B28 B#00000000 T4}%{F- B- T-}"
 else
-  echo "%{F#F75341 B#00000000 T4}%{B- F- T-}%{F#FCE8C3 B#F75341} VPN  Dis %{F- B-}%{F#F75341 B#00000000 T4}%{F- B- T-}"
+  echo "%{F#2D2B28 B#00000000 T4}%{B- F- T-}%{F#F75341 B#2D2B28} VPN %{F- B-}%{F#2D2B28 B#00000000 T4}%{F- B- T-}"
 fi
