c0abb07fc5e51790a6c84bf632df307da85e501a
Daniel Berg <mail@roosta.sh>
2017-03-27 08:38:37 +0200

    Increase icon size

--- conf/polybar/.config/polybar/vpn.sh
+++ conf/polybar/.config/polybar/vpn.sh
@@ -1,6 +1,6 @@
 vpn=$(pgrep -x openvpn)
 if [[ -n $vpn ]]; then
-  echo "%{F#2D2B28 B#00000000 T4}%{B- F- T-}%{F#98BC37 B#2D2B28} VPN %{F- B-}%{F#2D2B28 B#1C1B19 T4}%{F- B- T-}"
+  echo "%{F#2D2B28 B#00000000 T4}%{B- F- T-}%{F#98BC37 B#2D2B28}%{T3}%{T-} VPN %{F- B-}%{F#2D2B28 B#1C1B19 T4}%{F- B- T-}"
 else
-  echo "%{F#2D2B28 B#00000000 T4}%{B- F- T-}%{F#F75341 B#2D2B28} VPN %{F- B-}%{F#2D2B28 B#1C1B19 T4}%{F- B- T-}"
+  echo "%{F#2D2B28 B#00000000 T4}%{B- F- T-}%{F#F75341 B#2D2B28}%{T3}%{T-} VPN %{F- B-}%{F#2D2B28 B#1C1B19 T4}%{F- B- T-}"
 fi
