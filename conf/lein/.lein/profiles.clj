3fcf01079741f6ec501045cdf7d98159f1ab9441
Daniel Berg <mail@roosta.sh>
2017-04-04 17:19:50 +0200

    Cleanup profiles.clj

--- conf/lein/.lein/profiles.clj
+++ conf/lein/.lein/profiles.clj
@@ -1,27 +1,44 @@
 {:user {:jvm-opts ^:replace ["-Xmx512M"]
-        :dependencies [[com.cemerick/piggieback "0.2.1"]
+        :dependencies [;; nREPL support for ClojureScript REPLs 
+                       [com.cemerick/piggieback "0.2.1"]
+
+                       ;; alternative pprinter
                        ; [mvxcvi/puget "1.0.1"]
-                       [org.clojure/tools.nrepl "0.2.12"]
+
+                       ;; namespace tools for user/repl
                        [org.clojure/tools.namespace "0.2.11"] ]
 
-        :plugins [[lein-ancient "0.6.10"]
+        :plugins [;; check deps for new versions
+                  [lein-ancient "0.6.10"]
+
+                  ;; clojure syntax checking
                   [jonase/eastwood "0.2.3"]
+
+                  ;; generate page from doc-strings
                   [lein-codox "0.10.3"]
+
+                  ;; pretty print
                   [lein-pprint "1.1.2"]
+
+                  ;; clojure/script syntax checking
                   [lein-kibit "0.1.3"]]}
 
  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
  :repl {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                  [venantius/ultra "0.5.1"]
-                  ]
-        :ultra {:color-scheme :solarized_dark}
-        :dependencies [
-                       ; [alembic "0.3.2"]
-                       ; [acyclic/squiggly-clojure "0.1.7"]
-                       ]}
 
- :android-common {:android {:sdk-path "/home/roosta/lib/android-sdk-linux"}}
- :android-user {:dependencies [[cider/cider-nrepl "0.15.0-SNAPSHOT"]]
-                :android {:aot-exclude-ns ["cider.nrepl.middleware.util.java.parser"
-                                           "cider.nrepl" "cider-nrepl.plugin"]}}}
+                  ;; syntax highlight terminal repl
+                  [venantius/ultra "0.5.1"]
+
+                  ;; flycheck-clojure support for cider 'attach'
+                  [acyclic/squiggly-clojure "0.1.8" :exclusions [org.clojure/tools.reader]]
+
+                  ;; cider setup
+                  [refactor-nrepl "2.2.0"]]
+
+        :ultra {:color-scheme :solarized_dark}
+
+        :dependencies [;; cider setup
+                       [org.clojure/tools.nrepl "0.2.12"]
+                       ;; cider setup
+                       [alembic "0.3.2"]]}}
