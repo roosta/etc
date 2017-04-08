211e1150db032705ef541096c7aa4e8d92ae4a88
Daniel Berg <mail@roosta.sh>
2017-04-08 22:05:32 +0200

    Update pkg, vpn modules

--- conf/polybar/.config/polybar/pkg.sh
+++ conf/polybar/.config/polybar/pkg.sh
@@ -4,7 +4,7 @@ aur=$(cower -u | wc -l)
 
 check=$((pac + aur))
 if [[ "$check" != "0" ]]; then
-  echo "$pac/$aur %{F#0AAEB3 T5}%{F- T-}"
+  echo "$pac/$aur%{T5}%{T-}"
 else
-  echo "$pac/$aur %{T5}%{T-}"
+  echo "$pac/$aur%{T5}%{T-}"
 fi
