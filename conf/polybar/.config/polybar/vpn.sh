7057ad542614269b85c32d8d203622c9e6504112
Daniel Berg <mail@roosta.sh>
2017-04-11 20:18:03 +0200

    Update misc

--- conf/polybar/.config/polybar/vpn.sh
+++ conf/polybar/.config/polybar/vpn.sh
@@ -1,6 +1,6 @@
 tunnel=$(ip link show|awk '/(tun|pon)/{print substr($2, 1, 4)}')
 if [[ ! -z $tunnel ]]; then
-  echo "VPN: %{F#98BC37}Connected%{F-}"
+  echo "%{F#98BC37}VPN%{F-}"
 else
-  echo "VPN: %{F#F75341}Disconnected!%{F-}"
+  echo "%{F#F75341}No VPN!%{F-}"
 fi
