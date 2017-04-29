0ee8de21d4e95b47fb8cd691c8c9ef13fc8b0e1a
Daniel Berg <mail@roosta.sh>
2017-04-29 21:45:54 +0200

    Add keyboard-layout script

---
+++ conf/polybar/.config/polybar/layout.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# fix th
+# source: http://unix.stackexchange.com/a/27688
+case "$(xset -q|grep LED| awk '{ print $10 }')" in
+  "00000000"|"00000002"|"00000003") KBD="US" ;;
+  "00001000"|"00001002"|"00001003") KBD="%{F#F75341}NO%{F-}" ;;
+  *) KBD="unknown" ;;
+esac
+
+case "$(xset -q | grep Caps|awk '{print $4}')" in
+  "off") CAPS="";;
+  "on") CAPS="%{F#FCE8C3 B#F75341} Caps Lock %{F- B-}";;
+  *) CAPS="Something went wrong";;
+esac
+
+echo "$KBD $CAPS"
