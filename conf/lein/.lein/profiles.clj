a9d4394bacbc0ab8cab9c0c6125d16842c06f6a9
Daniel Berg <mail@roosta.sh>
2017-11-13 11:52:52 +0100

    Remove cider middleware test, use defaults

--- conf/lein/.lein/profiles.clj
+++ conf/lein/.lein/profiles.clj
@@ -29,25 +29,7 @@
 
  :repl-options {; :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]
                 ; :init (set! *print-length* 100)}
-                :nrepl-middleware [cider.nrepl.middleware.apropos/wrap-apropos
-                                   cider.nrepl.middleware.classpath/wrap-classpath
-                                   cider.nrepl.middleware.complete/wrap-complete
-                                   cider.nrepl.middleware.debug/wrap-debug
-                                   cider.nrepl.middleware.format/wrap-format
-                                   cider.nrepl.middleware.info/wrap-info
-                                   cider.nrepl.middleware.inspect/wrap-inspect
-                                   cider.nrepl.middleware.macroexpand/wrap-macroexpand
-                                   cider.nrepl.middleware.ns/wrap-ns
-                                   cider.nrepl.middleware.pprint/wrap-pprint
-                                   cider.nrepl.middleware.pprint/wrap-pprint-fn
-                                   cider.nrepl.middleware.refresh/wrap-refresh
-                                   cider.nrepl.middleware.resource/wrap-resource
-                                   cider.nrepl.middleware.stacktrace/wrap-stacktrace
-                                   cider.nrepl.middleware.test/wrap-test
-                                   cider.nrepl.middleware.trace/wrap-trace
-                                   cider.nrepl.middleware.out/wrap-out
-                                   cider.nrepl.middleware.undef/wrap-undef
-                                   cider.nrepl.middleware.version/wrap-version]}
+                :nrepl-middleware []}
 
  :repl {:plugins [[cider/cider-nrepl "0.16.0-SNAPSHOT"]
 
