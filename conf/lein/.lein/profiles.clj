b2b53d21053834688b4316096da5319a908e1e36
Daniel Berg <mail@roosta.sh>
2017-11-24 17:28:17 +0100

    Clean out all unneeded

--- conf/lein/.lein/profiles.clj
+++ conf/lein/.lein/profiles.clj
@@ -1,13 +1,5 @@
 {:user {:dependencies [;; nREPL support for ClojureScript REPLs 
-                       ; [com.cemerick/piggieback "0.2.2"]
-
-                       ;; alternative pprinter
-                       ; [mvxcvi/puget "1.0.1"]
-
-                       [org.clojure/tools.nrepl "0.2.13"]
-
-                       ;; namespace tools for user/repl
-                       [org.clojure/tools.namespace "0.3.0-alpha4"] ]
+                       [com.cemerick/piggieback "0.2.2"]]
 
         :plugins [;; check deps for new versions
                   [lein-ancient "0.6.12"]
@@ -27,29 +19,4 @@
                   ;; clojure/script syntax checking
                   [lein-kibit "0.1.6-beta2"]]}
 
- :repl-options {; :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]
-                ; :init (set! *print-length* 100)}
-                :nrepl-middleware []}
-
- :repl {:plugins [
-
-                  ;; syntax highlight terminal repl
-                  [venantius/ultra "0.5.1"]
-
-                  ;; flycheck-clojure support for cider 'attach'
-                  ; [acyclic/squiggly-clojure "0.1.9-SNAPSHOT" :exclusions [org.clojure/tools.reader]]
-
-                  ;; cider setup
-                  ; [cider/cider-nrepl "0.16.0-SNAPSHOT"]
-                  ; [refactor-nrepl "2.4.0-SNAPSHOT"]
-                  
-                  ]
-
-        :ultra {:color-scheme :solarized_dark}
-
-        :dependencies [;; cider setup
-                       [org.clojure/tools.nrepl "0.2.13"]
-                       ;; cider setup
-                       ; [alembic "0.3.2"]
-                       
-                       ]}}
+ :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
