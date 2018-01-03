d8fdf68bde6f62b77eeabf1e9e6d751112c0abf5
Daniel Berg <mail@roosta.sh>
2018-01-03 15:31:47 +0100

    Fix fshow date

--- conf/zsh/.zsh.d/plugin_conf/fzf.zsh
+++ conf/zsh/.zsh.d/plugin_conf/fzf.zsh
@@ -181,8 +181,8 @@ fcoc() {
 # fshow - git commit browser
 fshow() {
   git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf-tmux --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --format="%C(auto)%h%d %s %C(black)%C(white)%cr" "$@" |
+  fzf-tmux --ansi --no-sort --tiebreak=index --bind=ctrl-s:toggle-sort \
     --bind "ctrl-m:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
                 xargs -I % sh -c 'git show --color=always % | vimpager') << 'FZF-EOF'
