febfa73678e45c81dcfb9fd7a0d46b7ca7f4aef9
Daniel Berg <mail@roosta.sh>
2018-01-12 21:06:42 +0100

    Upgrade plugins

--- conf/lein/.lein/profiles.clj
+++ conf/lein/.lein/profiles.clj
@@ -2,22 +2,22 @@
                        [com.cemerick/piggieback "0.2.2"]]
 
         :plugins [;; check deps for new versions
-                  [lein-ancient "0.6.12"]
+                  [lein-ancient "0.6.15"]
 
                   ;; cider debugging
                   [com.billpiel/sayid "0.0.15"]
 
                   ;; clojure syntax checking
-                  [jonase/eastwood "0.2.4"]
+                  [jonase/eastwood "0.2.6-beta2"]
 
                   ;; generate page from doc-strings
                   [lein-codox "0.10.3"]
 
                   ;; pretty print
-                  [lein-pprint "1.1.2"]
+                  [lein-pprint "1.2.0"]
 
                   ;; clojure/script syntax checking
-                  [lein-kibit "0.1.6-beta2"]]}
+                  [lein-kibit "0.1.6"]]}
 
  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
  :figwheel {:nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]}
