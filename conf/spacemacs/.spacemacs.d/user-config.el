ebc98a18e0ad5239f21c6a5740f6bacd917a3a67
Daniel Berg <mail@roosta.sh>
2018-01-19 12:16:14 +0100

    Diff emacs conf, remove sayid init

--- conf/spacemacs/.spacemacs.d/user-config.el
+++ conf/spacemacs/.spacemacs.d/user-config.el
@@ -91,8 +91,8 @@
  )
 
 ;; load sayid keybindings for clojure-mode buffers
-(eval-after-load 'clojure-mode
-  '(sayid-setup-package))
+;; (eval-after-load 'clojure-mode
+;;   '(sayid-setup-package))
 
 (add-hook 'cider-repl-mode-hook #'evil-cleverparens-mode)
 (add-hook 'cider-repl-mode-hook #'eldoc-mode)
