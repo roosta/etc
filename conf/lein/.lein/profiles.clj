777e5b081702e7ffc78fd0cb44ff7a2b77ec85b2
Daniel Berg <mail@roosta.sh>
2018-02-27 15:43:13 +0100

    Remove sayid (cider injects), exclude clojure

--- conf/lein/.lein/profiles.clj
+++ conf/lein/.lein/profiles.clj
@@ -7,10 +7,10 @@
                   [lein-ancient "0.6.15"]
 
                   ;; cider debugging
-                  [com.billpiel/sayid "0.0.15"]
+                  ; [com.billpiel/sayid "0.0.15"]
 
                   ;; clojure syntax checking
-                  [jonase/eastwood "0.2.6-beta2"]
+                  [jonase/eastwood "0.2.6-beta2" :exclusions [org.clojure/clojure]]
 
                   ;; generate page from doc-strings
                   [lein-codox "0.10.3"]
@@ -25,7 +25,7 @@
                   [lein-pprint "1.2.0"]
 
                   ;; clojure/script syntax checking
-                  [lein-kibit "0.1.6"]]}
+                  [lein-kibit "0.1.6" :exclusions [org.clojure/clojure]]]}
 
  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
  :figwheel {:nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]}
