23d7ffcc22e47c0f94dd564472fc9954f12a45b3
Daniel Berg <mail@roosta.sh>
2018-04-10 21:13:35 +0200

    Add vifzf alias

--- conf/zsh/.zsh.d/aliases.zsh
+++ conf/zsh/.zsh.d/aliases.zsh
@@ -76,6 +76,7 @@ alias virofi="${EDITOR:-vim} ~/etc/templates/rofi/config.rofi"
 alias vijoker="${EDITOR:-vim} ~/.joker"
 alias vidunst="${EDITOR:-vim} ~/etc/templates/dunst/dunstrc"
 alias pacorg="${EDITOR:-vim} ~/org/pacman.org"
+alias vifzf="${EDITOR:-vim} ~/.zsh.d/plugin_conf/fzf.zsh"
 
 #}}}
 ## FIND {{{1
