265513c991f92369d9a34dddc9bbd43f24efc17d
Daniel Berg <mail@roosta.sh>
2018-04-17 14:06:37 +0200

    Fetch srcery from melpa

--- conf/spacemacs/.spacemacs.d/init.el
+++ conf/spacemacs/.spacemacs.d/init.el
@@ -98,6 +98,7 @@ This function should only modify configuration layer settings."
    '(arduino-mode
      ;; flycheck-clojure
      toc-org
+     srcery-theme
      paren-face
      xclip
      flycheck-joker
@@ -105,7 +106,8 @@ This function should only modify configuration layer settings."
      kibit-helper
      ;; evil-smartparens
      (evil-terminal-cursor-changer :location "~/src/evil-terminal-cursor-changer")
-     (srcery-theme :location "~/src/emacs-srcery"))
+     ;; (srcery-theme :location "~/src/emacs-srcery")
+     )
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
