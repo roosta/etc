91950374e03f8c44806fd97437a9351314326e15
Daniel Berg <mail@roosta.sh>
2018-05-04 09:48:34 +0200

    Cleanup

--- conf/spacemacs/.spacemacs.d/user-config.el
+++ conf/spacemacs/.spacemacs.d/user-config.el
@@ -154,18 +154,6 @@
 ;; Mutt support.
 (setq auto-mode-alist (append '(("/tmp/mutt.*" . mail-mode)) auto-mode-alist))
 
-;; (defadvice terminal-init-screen
-;;     ;; The advice is named `tmux', and is run before `terminal-init-screen' runs.
-;;     (before tmux activate)
-;;   ;; Docstring.  This describes the advice and is made available inside emacs;
-;;   ;; for example when doing C-h f terminal-init-screen RET
-;;   "Apply xterm keymap, allowing use of keys passed through tmux."
-;;   ;; This is the elisp code that is run before `terminal-init-screen'.
-;;   (if (getenv "TMUX")
-;;       (let ((map (copy-keymap xterm-function-map)))
-;;         (set-keymap-parent map (keymap-parent input-decode-map))
-;;         (set-keymap-parent input-decode-map map))))
-
 ;; ----------------------------------------------------
 ;; clojure
 ;; ----------------------------------------------------
