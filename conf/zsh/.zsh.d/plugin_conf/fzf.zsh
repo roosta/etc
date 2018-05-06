7a62987465fcbf7f2c5960c7866011cb2d8698c9
Daniel Berg <mail@roosta.sh>
2018-05-06 16:41:19 +0200

    Add e, v, and o functions, remove fo

--- conf/zsh/.zsh.d/plugin_conf/fzf.zsh
+++ conf/zsh/.zsh.d/plugin_conf/fzf.zsh
@@ -86,27 +86,27 @@ fkill() {
 # OPENING FILES {{{
 # ---------------
 
-# fe [FUZZY PATTERN] - Open the selected file with the default editor
-#   - Bypass fuzzy finder if there's only one match (--select-1)
-#   - Exit if there's no match (--exit-0)
-fe() {
+# Fuzzy match file and open with emacsclient
+e() {
   local file
-  file=$(fzf-tmux --query="$1" --select-1 --exit-0)
-  [ -n "$file" ] && ${EDITOR:-vim} "$file"
+  file=$(fzf-tmux --query="$1")
+  [ -n "$file" ] && emacsclient -nw "$file"
 }
 
-# Modified version where you can press
-#   - CTRL-O to open with `open` command,
-#   - CTRL-E or Enter key to open with the $EDITOR
-fo() {
-  local out file key
-  out=$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)
-  key=$(head -1 <<< "$out")
-  file=$(head -2 <<< "$out" | tail -1)
-  if [ -n "$file" ]; then
-    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
-  fi
+# Fuzzy match file and open with vim
+v() {
+  local file
+  file=$(fzf-tmux --query="$1")
+  [ -n "$file" ] && vim "$file"
 }
+
+# To to org folder and open match
+o() {
+  local file
+  cd ~/org && file=$(fzf-tmux --query="$1")
+  [ -n "$file" ] && emacsclient -nw "$file"
+}
+
 #}}}
 # GIT {{{
 # ---------
