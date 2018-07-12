71825556eb3c4519ec9405860fb58409f5cc8ffa
Daniel Berg <mail@roosta.sh>
2018-07-12 14:10:14 +0200

    Remove cider, piggieback from profile

--- conf/lein/.lein/profiles.clj
+++ conf/lein/.lein/profiles.clj
@@ -1,12 +1,4 @@
-{:user {:dependencies [;; nREPL support for ClojureScript REPLs 
-                       [cider/piggieback "0.3.6"]
-                       [org.clojure/tools.nrepl "0.2.13"]
-                       ; [com.bhauman/rebel-readline "0.1.2"]
-                       ]
-
-        ; :aliases {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"] }
-
-        :plugins [;; check deps for new versions
+{:user {:plugins [;; check deps for new versions
                   [lein-ancient "0.6.15"]
 
                   ;; clojure syntax checking
@@ -22,6 +14,4 @@
                   [lein-pprint "1.2.0"]
 
                   ;; clojure/script syntax checking
-                  [lein-kibit "0.1.6" :exclusions [org.clojure/clojure]]]}
-
- :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}
+                  [lein-kibit "0.1.6" :exclusions [org.clojure/clojure]]]}}
