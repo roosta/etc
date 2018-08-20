8cf484d667a32ff3e41bfe8e0965abdf78152ff6
Daniel Berg <mail@roosta.sh>
2018-08-20 10:25:43 +0200

    Use fzf-tmux for ef function

--- conf/zsh/.zsh.d/plugin_conf/fzf.zsh
+++ conf/zsh/.zsh.d/plugin_conf/fzf.zsh
@@ -119,14 +119,14 @@ o() {
 # vf - fuzzy open with vim from anywhere
 # ex: vf word1 word2 ... (even part of a file name)
 # zsh autoload function
-vf() {
+ef() {
   local files
 
-  files=(${(f)"$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf --read0 -0 -1 -m)"})
+  files=(${(f)"$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf-tmux --read0 -0 -1 -m)"})
 
   if [[ -n $files ]]
   then
-     vim -- $files
+     emacsclient -nw -- $files
      print -l $files[1]
   fi
 }
