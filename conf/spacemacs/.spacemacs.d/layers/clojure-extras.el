e232daefdee9bbaec6ec8c0ed8bba09fc2a24592
Daniel Berg <mail@roosta.sh>
2018-09-10 20:42:31 +0200

    Add missing arg to cider-jack-in

--- conf/spacemacs/.spacemacs.d/layers/clojure-extras.el
+++ conf/spacemacs/.spacemacs.d/layers/clojure-extras.el
@@ -4,6 +4,6 @@
            (lein-params (concat "with-profile +" profile " repl :headless")))
     (message "lein-params set to: %s" lein-params)
     (set-variable 'cider-lein-parameters lein-params)
-    (cider-jack-in)))
+    (cider-jack-in nil)))
 
 (provide 'clojure-extras)
