716dafe6aa242a02e34296eef149fb45ad598722
Daniel Berg <mail@roosta.sh>
2018-11-10 18:49:04 +0100

    Indent

--- conf/spacemacs/.spacemacs.d/init.el
+++ conf/spacemacs/.spacemacs.d/init.el
@@ -497,10 +497,10 @@ It should only modify the values of Spacemacs settings."
 
 (defun dotspacemacs/user-env ()
   "Environment variables setup.
-  This function defines the environment variables for your Emacs session. By
-  default it calls `spacemacs/load-spacemacs-env' which loads the environment
-  variables declared in `~/.spacemacs.env' or `~/.spacemacs.d/.spacemacs.env'.
-  See the header of this file for more information."
+This function defines the environment variables for your Emacs session. By
+default it calls `spacemacs/load-spacemacs-env' which loads the environment
+variables declared in `~/.spacemacs.env' or `~/.spacemacs.d/.spacemacs.env'.
+See the header of this file for more information."
   (spacemacs/load-spacemacs-env))
 
 (defun dotspacemacs/user-init ()
