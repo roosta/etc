9c0e8b78d5f05985b7da1b36637f667372475799
Daniel Berg <mail@roosta.sh>
2018-12-20 14:23:00 +0100

    Stop excluding git gutter

--- conf/spacemacs/.spacemacs.d/init.el
+++ conf/spacemacs/.spacemacs.d/init.el
@@ -128,7 +128,7 @@ This function should only modify configuration layer settings."
    dotspacemacs-frozen-packages '()
 
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages '(git-gutter rainbow-delimiters git-gutter+)
+   dotspacemacs-excluded-packages '(rainbow-delimiters)
 
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
