3a8aefac884cee7f4ab59d13c8b521872260bd82
Daniel Berg <mail@roosta.sh>
2019-04-21 15:54:27 +0200

    Add evil-surround

--- conf/emacs/.emacs.d/lisp/init-evil.el
+++ conf/emacs/.emacs.d/lisp/init-evil.el
@@ -23,6 +23,9 @@
              :config
              (evil-collection-init))
 
+(use-package evil-surround
+             :ensure t
+             :config (global-evil-surround-mode))
 
 (provide 'init-evil)
 ;;; init-evil.el ends here
