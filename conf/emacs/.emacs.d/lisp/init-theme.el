c1594a2ed629d26d5e9d7c2303168a25b8094e03
Daniel Berg <mail@roosta.sh>
2019-04-21 14:15:51 +0200

    Add reapply theme, and add init hook

--- conf/emacs/.emacs.d/lisp/init-theme.el
+++ conf/emacs/.emacs.d/lisp/init-theme.el
@@ -5,10 +5,19 @@
 (use-package srcery-theme
              :load-path "~/src/srcery-emacs"
              :init
-             (setq srcery-transparent-background t)) 
+             (setq srcery-transparent-background t))
 
-(setq-default custom-enabled-themes '(srcery-theme))
+(setq-default custom-enabled-themes '(srcery))
+
+; Ensure that themes will be applied even if they have not been customized
+(defun reapply-themes ()
+  "Forcibly load the themes listed in `custom-enabled-themes'."
+  (dolist (theme custom-enabled-themes)
+    (unless (custom-theme-p theme)
+      (load-theme theme)))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+
+(add-hook 'after-init-hook 'reapply-themes)
 
 (provide 'init-theme)
-
 ;;; init-themes.el ends here
