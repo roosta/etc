63198694c324594c636cdef4e99e3757b8b6499e
Daniel Berg <mail@roosta.sh>
2019-04-25 15:41:22 +0200

    Use versioned directory for elpa

--- conf/emacs/.emacs.d/lisp/init-elpa.el
+++ conf/emacs/.emacs.d/lisp/init-elpa.el
@@ -4,11 +4,11 @@
 
 (require 'package)
 
-;;; Install into separate package dirs for each Emacs version, to prevent bytecode incompatibility
-					; (let ((versioned-package-dir
-					;        (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
-					;                          user-emacs-directory)))
-					;   (setq package-user-dir versioned-package-dir))
+;; Install into separate package dirs for each Emacs version, to prevent bytecode incompatibility
+(let ((versioned-package-dir
+        (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
+                          user-emacs-directory)))
+  (setq package-user-dir versioned-package-dir))
 
 ;;; Melpa quick start
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
