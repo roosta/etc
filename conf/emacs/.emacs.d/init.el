09c4b5d462e9e5a0b5d6ff3a9acebf5ffef4026d
Daniel Berg <mail@roosta.sh>
2019-04-25 16:19:39 +0200

    Diminish which-key-mode

--- conf/emacs/.emacs.d/init.el
+++ conf/emacs/.emacs.d/init.el
@@ -49,7 +49,10 @@
 (use-package diminish :ensure t)
 (use-package dash :ensure t)
 (use-package s :ensure t)
-(use-package which-key :ensure t :config (which-key-mode))
+(use-package which-key
+  :ensure t
+  :config (which-key-mode)
+  :diminish which-key-mode)
 (use-package general :ensure t)
 
 (require 'init-frame-hooks)
