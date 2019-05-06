04041b4aeded164e233fbebce9a040ccf28b92ae
Daniel Berg <mail@roosta.sh>
2019-05-06 11:04:44 +0200

    Add melpa-stable to package archives

--- conf/emacs/.emacs.d/lisp/init-elpa.el
+++ conf/emacs/.emacs.d/lisp/init-elpa.el
@@ -23,7 +23,7 @@ There are two things you can do about this warning:
 2. Remove this warning from your init file so you won't see it again."))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
