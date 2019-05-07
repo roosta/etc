b702494b888244700f5f55a3986dc9f0ec5af1c7
Daniel Berg <mail@roosta.sh>
2019-05-07 13:03:32 +0200

    Change undo and backup folders to use emacs user directory

--- conf/emacs/.emacs.d/lisp/init-backup.el
+++ conf/emacs/.emacs.d/lisp/init-backup.el
@@ -3,15 +3,23 @@
 
 ;;; Code:
 
+;; define and create backup directory
+(defvar emacs-backup-directory (expand-file-name "var/backup" user-emacs-directory))
+(make-directory emacs-backup-directory t)
+
 ;; https://www.emacswiki.org/emacs/BackupDirectory
 (setq
-   backup-by-copying t      ; don't clobber symlinks
-   backup-directory-alist
-    '(("." . "~/var/emacs/backup/"))    ; don't litter my fs tree
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)       ; use versioned backups
+  backup-by-copying t      ; don't clobber symlinks
+  backup-directory-alist
+  `(("." . ,emacs-backup-directory))    ; don't litter my fs tree
+  delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t ; use versioned backups
+  auto-save-list-file-prefix
+  (concat emacs-backup-directory ".auto-saves-")
+  auto-save-file-name-transforms
+  `((".*" ,emacs-backup-directory t)))
 
 
 (provide 'init-backup)
--- conf/emacs/.emacs.d/lisp/init-undo-tree.el
+++ conf/emacs/.emacs.d/lisp/init-undo-tree.el
@@ -2,9 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
+(defvar emacs-undo-tree-history-directory (expand-file-name "var/undo" user-emacs-directory))
+(make-directory emacs-undo-tree-history-directory t)
+
+(defvar undo-tree-auto-save-history)
+(defvar undo-tree-history-directory-alist)
 (setq undo-tree-auto-save-history t)
-(setq undo-tree-history-directory-alist '(("." . "~/var/emacs/undo")))
+(setq undo-tree-history-directory-alist `(("." . ,emacs-undo-tree-history-directory)))
 (diminish 'undo-tree-mode)
 
 (provide 'init-undo-tree)
-;;; init-themes.el ends here
+;;; init-undo-tree.el ends here
