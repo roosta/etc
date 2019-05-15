582aab5829c8e45652dab7398198e4f22748e0de
Daniel Berg <mail@roosta.sh>
2019-05-15 21:17:13 +0200

    Add org mode keybindings, settings

--- conf/emacs/.emacs.d/lisp/init-org.el
+++ conf/emacs/.emacs.d/lisp/init-org.el
@@ -1,9 +1,14 @@
 ;;; init-org.el --- Org-mode configuration           -*- lexical-binding: t; -*-
+
 ;;; Commentary:
+;;; Heavily inspired by Spacemacs
+;;; https://github.com/syl20bnr/spacemacs/tree/master/layers/%2Bemacs/org
 
 ;;; Code:
 
 (require 'init-utils)
+(require 'org)
+(require 'org-capture)
 
 (defvar org-export-with-drawers)
 (defvar org-clock-persist)
@@ -17,6 +22,19 @@
 (defvar org-capture-templates)
 (defvar org-duration-format)
 
+;;----------------------------------------------------------------------------
+;; Emphasize macro
+;; Source: https://github.com/syl20bnr/spacemacs/blob/8c0b8c3440a33df2c291088c47e75fd5e4fbbb61/layers/%2Bemacs/org/packages.el#L142
+;;----------------------------------------------------------------------------
+(defmacro roosta/org-emphasize (fname char)
+  "Make function for setting the emphasis in org mode.
+Pass FNAME as the function name and CHAR as the character."
+        `(defun ,fname () (interactive)
+                (org-emphasize ,char)))
+
+;;----------------------------------------------------------------------------
+;; Preferences
+;;----------------------------------------------------------------------------
 (setq
  ;; I added this to export clock times in drawers
  org-export-with-drawers t
@@ -71,6 +89,9 @@
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+;;----------------------------------------------------------------------------
+;; Evil org
+;;----------------------------------------------------------------------------
 (use-package evil-org
   :after org
   :general
@@ -86,6 +107,148 @@
               (evil-org-set-key-theme)))
   (require 'evil-org-agenda)
   (evil-org-agenda-set-keys))
+
+;;----------------------------------------------------------------------------
+;; Keybindings
+;;----------------------------------------------------------------------------
+(general-define-key
+ :states  '(normal visual evilified)
+ :keymaps '(org-mode-map)
+ :prefix  ","
+ "'" #'org-edit-special
+ "c" #'org-capture
+ "d" #'org-deadline
+ "D" #'org-insert-drawer
+ "ee" #'org-export-dispatch
+ "f" #'org-set-effort
+ "P" #'org-set-property
+ ":" #'org-set-tags
+
+ "a" #'org-agenda
+ "b" #'org-tree-to-indirect-buffer
+ "A" #'org-archive-subtree
+ "l" #'org-open-at-point
+ "T" #'org-show-todo-tree
+
+ "." #'org-time-stamp
+ "!" #'org-time-stamp-inactive
+
+ ;; headings
+ "hi" #'org-insert-heading-after-current
+ "hI" #'org-insert-heading
+ "hs" #'org-insert-subheading
+
+ ;; More cycling options (timestamps, headlines, items, properties)
+ "L" #'org-shiftright
+ "H" #'org-shiftleft
+ "J" #'org-shiftdown
+ "K" #'org-shiftup
+
+ ;; Change between TODO sets
+ "C-S-l" #'org-shiftcontrolright
+ "C-S-h" #'org-shiftcontrolleft
+ "C-S-j" #'org-shiftcontroldown
+ "C-S-k" #'org-shiftcontrolup
+
+ ;; Subtree editing
+ "Sl" #'org-demote-subtree
+ "Sh" #'org-promote-subtree
+ "Sj" #'org-move-subtree-down
+ "Sk" #'org-move-subtree-up
+
+ ;; tables
+ "ta" #'org-table-align
+ "tb" #'org-table-blank-field
+ "tc" #'org-table-convert
+ "tdc" #'org-table-delete-column
+ "tdr" #'org-table-kill-row
+ "te" #'org-table-eval-formula
+ "tE" #'org-table-export
+ "th" #'org-table-previous-field
+ "tH" #'org-table-move-column-left
+ "tic" #'org-table-insert-column
+ "tih" #'org-table-insert-hline
+ "tiH" #'org-table-hline-and-move
+ "tir" #'org-table-insert-row
+ "tI" #'org-table-import
+ "tj" #'org-table-next-row
+ "tJ" #'org-table-move-row-down
+ "tK" #'org-table-move-row-up
+ "tl" #'org-table-next-field
+ "tL" #'org-table-move-column-right
+ "tn" #'org-table-create
+ "tN" #'org-table-create-with-table.el
+ "tr" #'org-table-recalculate
+ "ts" #'org-table-sort-lines
+ "ttf" #'org-table-toggle-formula-debugger
+ "tto" #'org-table-toggle-coordinate-overlays
+ "tw" #'org-table-wrap-region
+
+ ;; Multi-purpose keys
+ "," #'org-ctrl-c-ctrl-c
+ "*" #'org-ctrl-c-star
+ "RET" #'org-ctrl-c-ret
+ "-" #'org-ctrl-c-minus
+ "^" #'org-sort
+ "/" #'org-sparse-tree
+
+ "I" #'org-clock-in
+ "n" #'org-narrow-to-subtree
+ "N" #'widen
+ "O" #'org-clock-out
+ "q" #'org-clock-cancel
+ "R" #'org-refile
+ "s" #'org-schedule
+
+ ;; insertion of common elements
+ "ia" #'org-attach
+ "il" #'org-insert-link
+ "if" #'org-footnote-new
+
+ ;; region manipulation
+ "xb" (roosta/org-emphasize roosta/org-bold ?*)
+ "xc" (roosta/org-emphasize roosta/org-code ?~)
+ "xi" (roosta/org-emphasize roosta/org-italic ?/)
+ "xr" (roosta/org-emphasize roosta/org-clear ? )
+ "xs" (roosta/org-emphasize roosta/org-strike-through ?+)
+ "xu" (roosta/org-emphasize roosta/org-underline ?_)
+ "xv" (roosta/org-emphasize roosta/org-verbose ?=)
+ )
+
+(after-load 'org-capture
+  (general-define-key
+   :states  '(normal visual evilified)
+   :keymaps '(org-capture-mode-map)
+   :prefix  ","
+   "c" #'org-capture-finalize
+   "k" #'org-capture-kill
+   "a" #'org-capture-kill
+   "r" #'org-capture-refile
+   ))
+
+(after-load 'org-src
+  (general-define-key
+   :states  '(normal visual evilified)
+   :keymaps '(org-src-mode-map)
+   :prefix  ","
+   "c" #'org-edit-src-exit
+   "a" #'org-edit-src-abort
+   "k" #'org-edit-src-abort))
+
+(which-key-add-major-mode-key-based-replacements 'org-mode
+  ", e" "export"
+  ", x" "text"
+  ", h" "headings"
+  ", i" "insert"
+  ", S" "subtrees"
+  ", t" "tables"
+  ", td" "delete"
+  ", ti" "insert"
+  ", tt" "toggle")
+
+(general-def '(normal visual operator) org-mode-map
+      "C-j" nil
+      "C-k" nil)
 
 (provide 'init-org)
 ;;; init-org.el ends here
