653a4de5846ea380d224e7c7324c0116352ae63d
Daniel Berg <mail@roosta.sh>
2019-05-16 14:09:05 +0200

    Add magit, evil-magit

--- conf/emacs/.emacs.d/lisp/init-git.el
+++ conf/emacs/.emacs.d/lisp/init-git.el
@@ -5,6 +5,20 @@
 
 ;;; Code:
 
+(use-package magit
+  :general
+  ('(normal visual insert emacs) :prefix "SPC" :non-normal-prefix "C-SPC"
+   "gb"  #'magit-blame
+   "gfh" #'magit-log-buffer-file
+   "gm"  #'magit-dispatch-popup
+   "gs"  #'magit-status
+   "gS"  #'magit-stage-file
+   "gU"  #'magit-unstage-file))
+
+(use-package evil-magit
+  :after (magit))
+
+;; Use auto commit for org-mode
 (use-package git-auto-commit-mode)
 
 (provide 'init-git)
