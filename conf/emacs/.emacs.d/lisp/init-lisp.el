49b8ae9b827f7f262b8a9a1ae20ccb2aa9bb6924
Daniel Berg <mail@roosta.sh>
2019-05-20 15:29:34 +0200

    Use smartparens-config
    Must have removed it by accident

--- conf/emacs/.emacs.d/lisp/init-lisp.el
+++ conf/emacs/.emacs.d/lisp/init-lisp.el
@@ -7,6 +7,7 @@
 ;;----------------------------------------------------------------------------
 (use-package smartparens
   :config
+  (require 'smartparens-config)
   (smartparens-global-strict-mode))
 
 
