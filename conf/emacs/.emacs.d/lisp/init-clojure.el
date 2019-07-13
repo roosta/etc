05e64aefe9a2bed082080359435800ff43b29119
Daniel Berg <mail@roosta.sh>
2019-07-13 18:48:52 +0200

    Remove sharp quote from known to be defined spacemacs fns

--- conf/emacs/.emacs.d/lisp/init-clojure.el
+++ conf/emacs/.emacs.d/lisp/init-clojure.el
@@ -115,39 +115,39 @@
   "'"  #'cider-jack-in
   "\"" #'cider-jack-in-cljs
   "sb" #'cider-load-buffer
-  "sB" #'spacemacs/cider-send-buffer-in-repl-and-focus
+  "sB" 'spacemacs/cider-send-buffer-in-repl-and-focus
   "sc" #'cider-connect
   "sC" #'cider-find-and-clear-repl-output
-  "se" #'spacemacs/cider-send-last-sexp-to-repl
-  "sE" #'spacemacs/cider-send-last-sexp-to-repl-focus
-  "sf" #'spacemacs/cider-send-function-to-repl
-  "sF" #'spacemacs/cider-send-function-to-repl-focus
+  "se" 'spacemacs/cider-send-last-sexp-to-repl
+  "sE" 'spacemacs/cider-send-last-sexp-to-repl-focus
+  "sf" 'spacemacs/cider-send-function-to-repl
+  "sF" 'spacemacs/cider-send-function-to-repl-focus
   "si" #'cider-jack-in
   "sI" #'cider-jack-in-cljs
-  "sn" #'spacemacs/cider-send-ns-form-to-repl
-  "sN" #'spacemacs/cider-send-ns-form-to-repl-focus
+  "sn" 'spacemacs/cider-send-ns-form-to-repl
+  "sN" 'spacemacs/cider-send-ns-form-to-repl-focus
   "so" #'cider-repl-switch-to-other
   "sq" #'cider-quit
-  "sr" #'spacemacs/cider-send-region-to-repl
-  "sR" #'spacemacs/cider-send-region-to-repl-focus
+  "sr" 'spacemacs/cider-send-region-to-repl
+  "sR" 'spacemacs/cider-send-region-to-repl-focus
   "ss" #'cider-switch-to-repl-buffer
   "sx" #'cider-ns-refresh
 
   "Te" #'cider-enlighten-mode
-  "Tf" #'spacemacs/cider-toggle-repl-font-locking
-  "Tp" #'spacemacs/cider-toggle-repl-pretty-printing
+  "Tf" 'spacemacs/cider-toggle-repl-font-locking
+  "Tp" 'spacemacs/cider-toggle-repl-pretty-printing
   "Tt" #'cider-auto-test-mode
 
-  "ta" #'spacemacs/cider-test-run-all-tests
+  "ta" 'spacemacs/cider-test-run-all-tests
   "tb" #'cider-test-show-report
-  "tl" #'spacemacs/cider-test-run-loaded-tests
-  "tp" #'spacemacs/cider-test-run-project-tests
-  "tn" #'spacemacs/cider-test-run-ns-tests
-  "tr" #'spacemacs/cider-test-rerun-failed-tests
-  "tt" #'spacemacs/cider-test-run-focused-test
+  "tl" 'spacemacs/cider-test-run-loaded-tests
+  "tp" 'spacemacs/cider-test-run-project-tests
+  "tn" 'spacemacs/cider-test-run-ns-tests
+  "tr" 'spacemacs/cider-test-rerun-failed-tests
+  "tt" 'spacemacs/cider-test-run-focused-test
 
   "db" #'cider-debug-defun-at-point
-  "de" #'spacemacs/cider-display-error-buffer
+  "de" 'spacemacs/cider-display-error-buffer
   "dv" #'cider-inspect
 
   ;; refactorings from clojure-mode
