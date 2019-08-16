215595008813e4e4a2f5d95e72fdcb671ccc8907
Daniel Berg <mail@roosta.sh>
2019-08-16 19:47:14 +0200

    Unbind SPC from dired

--- conf/emacs/.emacs.d/lisp/init-dired.el
+++ conf/emacs/.emacs.d/lisp/init-dired.el
@@ -4,12 +4,16 @@
 ;;; Code:
 
 (require 'init-utils)
+(require 'dired)
 
 (general-def 'normal
   "-" #'dired-jump)
 
 (after-load 'diff-hl
   (add-hook 'dired-mode-hook 'diff-hl-dired-mode))
+
+(general-unbind 'normal dired-mode-map
+  "SPC")
 
 (provide 'init-dired)
 ;;; init-dired.el ends here
