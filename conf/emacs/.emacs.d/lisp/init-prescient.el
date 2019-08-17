ae2a711e327713302d99547d7b08c4da835cf25d
Daniel Berg <mail@roosta.sh>
2019-08-17 14:39:52 +0200

    Fix prescient issue where it would not work

--- conf/emacs/.emacs.d/lisp/init-prescient.el
+++ conf/emacs/.emacs.d/lisp/init-prescient.el
@@ -3,17 +3,20 @@
 ;;; Code:
 
 (use-package prescient
+  :demand t
   :commands
   (prescient-persist-mode)
   :config
   (prescient-persist-mode 1))
 
 (use-package company-prescient
+  :demand t
   :after (company prescient)
   :config
   (company-prescient-mode 1))
 
 (use-package ivy-prescient
+  :demand t
   :after (ivy prescient)
   :config
   (ivy-prescient-mode 1))
