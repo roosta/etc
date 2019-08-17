4142bbc0f88c3cb4611068bcbb1d6f927a5e0e9d
Daniel Berg <mail@roosta.sh>
2019-08-17 16:01:51 +0200

    Update docstrings

--- conf/emacs/.emacs.d/lisp/init-frame-hooks.el
+++ conf/emacs/.emacs.d/lisp/init-frame-hooks.el
@@ -4,9 +4,9 @@
 ;;; Code:
 
 (defvar after-make-console-frame-hooks '()
-  "Hooks to run after creating a new TTY frame")
+  "Hooks to run after creating a new TTY frame.")
 (defvar after-make-window-system-frame-hooks '()
-  "Hooks to run after creating a new window-system frame")
+  "Hooks to run after creating a new window-system frame.")
 
 (defun run-after-make-frame-hooks (frame)
   "Run configured hooks in response to the newly-created FRAME.
