d087d253d67b350f3e011bd1dd67ea5f727d3593
Daniel Berg <mail@roosta.sh>
2019-09-06 16:10:51 +0200

    Add web-mode
    Move sass/scss-mode

--- conf/emacs/.emacs.d/lisp/init-html.el
+++ conf/emacs/.emacs.d/lisp/init-html.el
@@ -2,14 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package sass-mode
-  :defer t
-  :mode ("\\.sass\\'" . sass-mode))
-
-(use-package scss-mode
-    :defer t
-    :mode ("\\.scss\\'" . scss-mode))
-
 ;;----------------------------------------------------------------------------
 ;; emmet
 ;;----------------------------------------------------------------------------
@@ -48,6 +40,91 @@
    "M-?" #'tagedit-convolute-tags
    "M-d" #'tagedit-kill
    "M-a" #'tagedit-kill-attribute))
+
+;;----------------------------------------------------------------------------
+;; CSS
+;;----------------------------------------------------------------------------
+(use-package sass-mode
+  :defer t
+  :mode ("\\.sass\\'" . sass-mode))
+
+(use-package scss-mode
+    :defer t
+    :mode ("\\.scss\\'" . scss-mode))
+
+(defun css-expand-statement ()
+  "Expand CSS block."
+  (interactive)
+  (save-excursion
+    (end-of-line)
+    (search-backward "{")
+    (forward-char 1)
+    (while (or (eobp) (not (looking-at "}")))
+      (let ((beg (point)))
+        (newline)
+        (search-forward ";")
+        (indent-region beg (point))
+        ))
+    (newline)))
+
+(defun css-contract-statement ()
+  "Contract CSS block."
+  (interactive)
+  (end-of-line)
+  (search-backward "{")
+  (while (not (looking-at "}"))
+    (join-line -1)))
+
+(general-define-key
+ :states  '(normal visual evilified)
+ :keymaps '(css-mode-map)
+ :prefix  ","
+ "zc" #'css-contract-statement
+ "zo" #'css-expand-statement)
+
+;;----------------------------------------------------------------------------
+;; Web-mode
+;;----------------------------------------------------------------------------
+(use-package web-mode
+  :mode
+  (("\\.phtml\\'"      . web-mode)
+   ("\\.tpl\\.php\\'"  . web-mode)
+   ("\\.twig\\'"       . web-mode)
+   ("\\.html\\'"       . web-mode)
+   ("\\.htm\\'"        . web-mode)
+   ("\\.[gj]sp\\'"     . web-mode)
+   ("\\.as[cp]x?\\'"   . web-mode)
+   ("\\.eex\\'"        . web-mode)
+   ("\\.erb\\'"        . web-mode)
+   ("\\.mustache\\'"   . web-mode)
+   ("\\.handlebars\\'" . web-mode)
+   ("\\.hbs\\'"        . web-mode)
+   ("\\.eco\\'"        . web-mode)
+   ("\\.ejs\\'"        . web-mode)
+   ("\\.djhtml\\'"     . web-mode))
+  :general
+  ('(normal visual evilified) web-mode-map :prefix ","
+   "eh" 'web-mode-dom-errors-show
+   "gb" 'web-mode-element-beginning
+   "gc" 'web-mode-element-child
+   "gp" 'web-mode-element-parent
+   "gs" 'web-mode-element-sibling-next
+   "hp" 'web-mode-dom-xpath
+   "rc" 'web-mode-element-clone
+   "rd" 'web-mode-element-vanish
+   "rk" 'web-mode-element-kill
+   "rr" 'web-mode-element-rename
+   "rw" 'web-mode-element-wrap
+   "z" 'web-mode-fold-or-unfold))
+
+(which-key-add-major-mode-key-based-replacements 'web-mode
+  ", e" "errors"
+  ", g" "goto"
+  ", h" "dom"
+  ", r" "refactor")
+
+(which-key-add-major-mode-key-based-replacements 'css-mode
+  ", z" "contract/expand")
 
 (provide 'init-html)
 ;;; init-html.el ends here
