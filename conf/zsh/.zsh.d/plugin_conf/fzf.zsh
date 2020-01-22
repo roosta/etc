227b26f076cbfc117b186d7be66d306070c969f0
Daniel Berg <mail@roosta.sh>
2020-01-22 20:26:15 +0100

    Revert fif and store as private
    For reference

--- conf/zsh/.zsh.d/plugin_conf/fzf.zsh
+++ conf/zsh/.zsh.d/plugin_conf/fzf.zsh
@@ -274,6 +274,13 @@ fif() {
       emacsclient -nw +$linum $file && popd
 }
 
+# using ripgrep combined with preview
+# find-in-file - usage: fif <searchTerm>
+_fif() {
+  if [ ! "$#" -gte 1 ]; then echo "Need a string to search for!"; return 1; fi
+  rg --files-with-matches --no-messages $1 | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 $1 || rg --ignore-case --pretty --context 10 $1 {}"
+}
+
 forg() {
   local match linum file;
   (
