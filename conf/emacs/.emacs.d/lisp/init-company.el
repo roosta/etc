5e0e310f561827ca4068e1de1bd2fb86b756f302
Daniel Berg <mail@roosta.sh>
2020-01-23 18:15:26 +0100

    Add sort tabnine defun

--- conf/emacs/.emacs.d/lisp/init-company.el
+++ conf/emacs/.emacs.d/lisp/init-company.el
@@ -40,11 +40,36 @@ Source: https://github.com/MatthewZMD/.emacs.d#company"
    "C-d" 'company-show-doc-buffer))
 
 (use-package company-tabnine
-  :demand t
+  :defer t
+  :hook
+  (lsp-after-open . (lambda ()
+                      (setq company-tabnine-max-num-results 3)
+                      (add-to-list 'company-transformers 'company//sort-by-tabnine t)
+                      (add-to-list 'company-backends '(company-lsp :with company-tabnine :separate))))
   :commands
   (company-tabnine)
   :config
-  (add-to-list 'company-backends #'company-tabnine))
+  ;; Enable TabNine on default
+  (add-to-list 'company-backends #'company-tabnine)
+  (defun company//sort-by-tabnine (candidates)
+    (if (or (functionp company-backend)
+            (not (and (listp company-backend) (memq 'company-tabnine company-backend))))
+        candidates
+      (let ((candidates-table (make-hash-table :test #'equal))
+            candidates-lsp
+            candidates-tabnine)
+        (dolist (candidate candidates)
+          (if (eq (get-text-property 0 'company-backend candidate)
+                  'company-tabnine)
+              (unless (gethash candidate candidates-table)
+                (push candidate candidates-tabnine))
+            (push candidate candidates-lsp)
+            (puthash candidate t candidates-table)))
+        (setq candidates-lsp (nreverse candidates-lsp))
+        (setq candidates-tabnine (nreverse candidates-tabnine))
+        (nconc (seq-take candidates-tabnine 3)
+               (seq-take candidates-lsp 6)))))
+  )
 
 (use-package company-lsp
   :commands company-lsp
