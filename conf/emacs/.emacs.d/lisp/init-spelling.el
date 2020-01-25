35ccb91925656e96885cc4dbacb810423a2bdac0
Daniel Berg <mail@roosta.sh>
2020-01-25 19:04:38 +0100

    Confirm before saving word

--- conf/emacs/.emacs.d/lisp/init-spelling.el
+++ conf/emacs/.emacs.d/lisp/init-spelling.el
@@ -35,11 +35,11 @@
   (defun roosta/flyspell-save-word ()
     "Save a word to personal dictionary"
     (interactive)
+    (y-or-n-p "Save word to dictionary? ")
     (let ((current-location (point))
           (word (flyspell-get-word)))
       (when (consp word)
-        (flyspell-do-correct 'save nil (car word) current-location (cadr word) (caddr word) current-location))))
-  )
+        (flyspell-do-correct 'save nil (car word) current-location (cadr word) (caddr word) current-location)))))
 
 (use-package define-word
   :commands
