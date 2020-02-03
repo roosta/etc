bafee4589a3146869614cabef67382e754e166f9
Daniel Berg <mail@roosta.sh>
2020-02-03 18:34:46 +0100

    Add cmake-mode via site-lisp utils

--- conf/emacs/.emacs.d/lisp/init-cpp.el
+++ conf/emacs/.emacs.d/lisp/init-cpp.el
@@ -5,6 +5,8 @@
 ;; https://www.reddit.com/r/emacs/comments/dlf0w7/whats_your_setup_for_cc_in_emacs/
 ;;; Code:
 
+(require 'init-site-lisp)
+
 (use-package lsp-mode :commands lsp)
 (use-package lsp-ui :commands lsp-ui-mode)
 
@@ -19,6 +21,13 @@
   (modern-c++-font-lock-global-mode)
   :diminish t
   :init (modern-c++-font-lock-global-mode t))
+
+;; CMake mode
+(ensure-lib-from-url
+ 'cmake-mode
+ "https://raw.githubusercontent.com/Kitware/CMake/master/Auxiliary/cmake-mode.el")
+
+(require 'cmake-mode)
 
 (provide 'init-cpp)
 ;;; init-cpp.el ends here
