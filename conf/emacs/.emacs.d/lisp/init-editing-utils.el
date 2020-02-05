4b1ec35d3311cd33ac001c284b6923012cd8e40e
Daniel Berg <mail@roosta.sh>
2020-02-05 19:30:43 +0100

    Header

--- conf/emacs/.emacs.d/lisp/init-editing-utils.el
+++ conf/emacs/.emacs.d/lisp/init-editing-utils.el
@@ -1,5 +1,26 @@
 ;;; init-editing-utils.el --- Editing utils and defaults  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Daniel Berg
+
+;; Author: Daniel Berg <mail@roosta.sh>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
+;;
+
 ;;; Code:
 
 ;;----------------------------------------------------------------------------
