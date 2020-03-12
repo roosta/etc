14a84ab08021f4bf98173eb95dea063c45401e75
Daniel Berg <mail@roosta.sh>
2020-03-12 12:55:13 +0100

    Add some spaces between regions

--- conf/emacs/.emacs.d/init.el
+++ conf/emacs/.emacs.d/init.el
@@ -5,17 +5,19 @@
 
 ;;; Commentary:
 ;;; Code:
+
 ;; load path {{{
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 ;;}}}
+
 ;; Adjust garbage collection thresholds during startup, and thereafter {{{
-;;----------------------------------------------------------------------------
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
       (init-gc-cons-threshold (* 128 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
   (add-hook 'emacs-startup-hook
             (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 ;;}}}
+
 ;; Bootstrap config {{{
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (defvar local-file (expand-file-name (concat "local/" (system-name) ".el") user-emacs-directory))
@@ -25,10 +27,12 @@
 (require 'init-global-config)
 ;; (require 'init-exec-path) ;; Set up $PATH
 ;;}}}
+
 ;; Variables configured via the interactive 'customize' interface {{{
 (when (file-exists-p custom-file)
   (load custom-file))
 ;;}}}
+
 ;; Load configs for specific features and modes {{{
 (use-package diminish)
 (use-package dash)
@@ -81,6 +85,7 @@
 ;; Run el-get sync to fetch all remote packages
 (el-get 'sync)
 ;;}}}
+
 ;; Allow access from Emacs client {{{
 (add-hook 'after-init-hook
           (lambda ()
@@ -89,9 +94,11 @@
                      (not (server-running-p)))
                 (server-start))))
 ;;}}}
+
 ;; Load host local variables {{{
 (when (file-exists-p local-file)
   (load local-file))
 ;;}}}
+
 (provide 'init)
 ;;; init.el ends here
