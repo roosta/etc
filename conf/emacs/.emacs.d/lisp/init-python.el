23a17b1685f54d19348506b788e285fd834b3a26
Daniel Berg <mail@roosta.sh>
2020-04-02 13:50:30 +0200

    Update spacemacs functions from develop instead of master

--- conf/emacs/.emacs.d/lisp/init-python.el
+++ conf/emacs/.emacs.d/lisp/init-python.el
@@ -28,7 +28,6 @@
   (require 'init-const))
 
 (use-package python-mode
-  :ensure nil
   :after flycheck
   :mode "\\.py\\'"
   :init
@@ -43,6 +42,8 @@
     ", mV" "pyvenv")
   :custom
   (python-indent-offset 4)
+  (python-shell-interpreter-args "-i")
+  (python-shell-interpreter "python")
   :general
   ('(normal visual evilified) python-mode-map :prefix ","
    "'"  'python-start-or-switch-repl
@@ -50,22 +51,46 @@
    "cC" 'spacemacs/python-execute-file-focus
    ;; "db" 'spacemacs/python-toggle-breakpoint
    ;; "ri" 'spacemacs/python-remove-unused-imports
-   "sB" 'python-shell-send-buffer-switch
-   "sb" 'python-shell-send-buffer
-   "sF" 'python-shell-send-defun-switch
-   "sf" 'python-shell-send-defun
-   "si" 'python-start-or-switch-repl
-   "sR" 'python-shell-send-region-switch
-   "sr" 'python-shell-send-region)
+   "sB" 'spacemacs/python-shell-send-buffer-switch
+   "sb" 'spacemacs/python-shell-send-buffer
+   "sF" 'spacemacs/python-shell-send-defun-switch
+   "sf" 'spacemacs/python-shell-send-defun
+   "si" 'spacemacs/python-start-or-switch-repl
+   "sR" 'spacemacs/python-shell-send-region-switch
+   "sr" 'spacemacs/python-shell-send-region)
   :config
+
+  (defun spacemacs/pyenv-executable-find (command)
+    "Find executable taking pyenv shims into account.
+If the executable is a system executable and not in the same path
+as the pyenv version then also return nil. This works around https://github.com/pyenv/pyenv-which-ext
+"
+    (if (executable-find "pyenv")
+        (progn
+          (let ((pyenv-string (shell-command-to-string (concat "pyenv which " command)))
+                (pyenv-version-names (split-string (string-trim (shell-command-to-string "pyenv version-name")) ":"))
+                (executable nil)
+                (i 0))
+            (if (not (string-match "not found" pyenv-string))
+                (while (and (not executable)
+                            (< i (length pyenv-version-names)))
+                  (if (string-match (elt pyenv-version-names i) (string-trim pyenv-string))
+                      (setq executable (string-trim pyenv-string)))
+                  (if (string-match (elt pyenv-version-names i) "system")
+                      (setq executable (string-trim (executable-find command))))
+                  (setq i (1+ i))))
+            executable))
+      (executable-find command)))
+
   (defun spacemacs/python-execute-file (arg)
     "Execute a python script in a shell."
     (interactive "P")
     ;; set compile command to buffer-file-name
     ;; universal argument put compile buffer in comint mode
     (let ((universal-argument t)
-          (compile-command (format "python %s" (file-name-nondirectory
-                                                buffer-file-name))))
+          (compile-command (format "%s %s"
+                                   (spacemacs/pyenv-executable-find python-shell-interpreter)
+                                   (shell-quote-argument (file-name-nondirectory buffer-file-name)))))
       (if arg
           (call-interactively 'compile)
         (compile compile-command t)
@@ -74,35 +99,38 @@
 
   (defun spacemacs/python-execute-file-focus (arg)
     "Execute a python script in a shell and switch to the shell buffer in
-`insert state'."
+ `insert state'."
     (interactive "P")
     (spacemacs/python-execute-file arg)
     (switch-to-buffer-other-window "*compilation*")
     (end-of-buffer)
     (evil-insert-state))
 
-  (defun python-shell-send-buffer-switch ()
+  (defun spacemacs/python-shell-send-buffer-switch ()
     "Send buffer content to shell and switch to it in insert mode."
     (interactive)
-    (python-shell-send-buffer)
-    (python-shell-switch-to-shell)
-    (evil-insert-state))
+    (let ((python-mode-hook nil))
+      (python-shell-send-buffer)
+      (python-shell-switch-to-shell)
+      (evil-insert-state)))
 
-  (defun python-shell-send-defun-switch ()
+  (defun spacemacs/python-shell-send-defun-switch ()
     "Send function content to shell and switch to it in insert mode."
     (interactive)
-    (python-shell-send-defun nil)
-    (python-shell-switch-to-shell)
-    (evil-insert-state))
+    (let ((python-mode-hook nil))
+      (python-shell-send-defun nil)
+      (python-shell-switch-to-shell)
+      (evil-insert-state)))
 
-  (defun python-shell-send-region-switch (start end)
+  (defun spacemacs/python-shell-send-region-switch (start end)
     "Send region content to shell and switch to it in insert mode."
     (interactive "r")
-    (python-shell-send-region start end)
-    (python-shell-switch-to-shell)
-    (evil-insert-state))
+    (let ((python-mode-hook nil))
+      (python-shell-send-region start end)
+      (python-shell-switch-to-shell)
+      (evil-insert-state)))
 
-  (defun python-start-or-switch-repl ()
+  (defun spacemacs/python-start-or-switch-repl ()
     "Start and/or switch to the REPL."
     (interactive)
     (let ((shell-process
@@ -120,7 +148,13 @@
       (unless shell-process
         (error "Failed to start python shell properly"))
       (pop-to-buffer (process-buffer shell-process))
-      (evil-insert-state))))
+      (evil-insert-state)))
+
+  (defun spacemacs/python-shell-send-region (start end)
+    "Send region content to shell and switch to it in insert mode."
+    (interactive "r")
+    (let ((python-mode-hook nil))
+      (python-shell-send-region start end))))
 
 (use-package lsp-python-ms
   :after lsp-mode python
