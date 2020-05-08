18680c44ff35c7286fb9f8eafbdbe83ff26bceab
Daniel Berg <mail@roosta.sh>
2020-05-08 10:49:11 +0200

    Leave in some commented fpath and autoloads

--- conf/zsh/.zsh.d/completion.zsh
+++ conf/zsh/.zsh.d/completion.zsh
@@ -3,6 +3,7 @@
 # ┗━┛━━┛┇ ┻  ┗━┛┛━┛┛ ┇┇  ┇━┛┻━┛ ┇ ┇┛━┛┇┗┛
 
 fpath=($HOME/.zsh.d/completion $fpath)
-fpath=($HOME/.zsh.d/plugins/zsh-completions/src $fpath)
+# fpath=($HOME/.zsh.d/plugins/zsh-completions/src $fpath)
 autoload -Uz compinit
 compinit -u
+# autoload -U ~/.zsh.d/completion/*(:t)
