f4ef95704dd213be47de86bc12a78d26b903ba25
Daniel Berg <mail@roosta.sh>
2020-05-10 15:10:21 +0200

    Make completion less strict

--- conf/zsh/.zsh.d/completion.zsh
+++ conf/zsh/.zsh.d/completion.zsh
@@ -7,3 +7,16 @@ fpath=($HOME/.zsh.d/completion $fpath)
 autoload -Uz compinit
 compinit -u
 # autoload -U ~/.zsh.d/completion/*(:t)
+
+# OPTIONS
+setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
+
+
+# Use caching to make completion for commands such as dpkg and apt usable.
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path "${HOME}/.zcompcache"
+
+
+# Case-insensitive (all), partial-word, and then substring completion.
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+unsetopt CASE_GLOB
