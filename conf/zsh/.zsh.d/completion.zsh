4d601d67bdab3ff7496df7a75d67c9bb56fa058a
Daniel Berg <mail@roosta.sh>
2020-05-15 12:37:59 +0200

    Enable fuzzy matching mistyped completions

--- conf/zsh/.zsh.d/completion.zsh
+++ conf/zsh/.zsh.d/completion.zsh
@@ -11,7 +11,6 @@ compinit -u
 # OPTIONS
 setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
 
-
 # Use caching to make completion for commands such as dpkg and apt usable.
 zstyle ':completion::complete:*' use-cache on
 zstyle ':completion::complete:*' cache-path "${HOME}/.zcompcache"
@@ -20,3 +19,8 @@ zstyle ':completion::complete:*' cache-path "${HOME}/.zcompcache"
 # Case-insensitive (all), partial-word, and then substring completion.
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 unsetopt CASE_GLOB
+
+# Fuzzy match mistyped completions.
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
