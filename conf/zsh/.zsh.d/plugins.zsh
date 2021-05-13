a006706c5564c81fbb92d66e11a7ab0cb62c7c94
Daniel Berg <mail@roosta.sh>
2021-05-13 13:28:48 +0200

    zsh: add zsh-vi-mode opts

--- conf/zsh/.zsh.d/plugins.zsh
+++ conf/zsh/.zsh.d/plugins.zsh
@@ -23,5 +23,11 @@ if  [[ -f $HOME/.dircolors ]]; then
 fi
 
 # }}}
+# zsh-vi-mode {{{
+
+ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
+ZVM_VI_HIGHLIGHT_BACKGROUND=red
+
+# }}}
 
 #  vim: set ts=2 sw=2 tw=0 fdm=marker et :
