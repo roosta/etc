09b3b2e76ec1e6f8fa2d7455ffcb59030c3801e8
Daniel Berg <mail@roosta.sh>
2021-09-30 18:22:24 +0200

    make: do git operations in parallel

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -4,40 +4,73 @@ error_msg() {
   exit 1
 }
 
-# take two args. Parent folder and repositories text file.
-# loop over each repo in text file, attempting to clone
-git_clone () {
-(( $# == 2 )) || error_msg
+git_clone() {
+  local repo="$1"
+  local out
+  out=$(git clone "$repo" 2>&1)
+  echo "$out"
+}
+export -f git_clone
 
-cd "$1" || exit
+git_pull() {
+  local dest="$1"
+  cd "$dest" && git pull && cd - || return
+}
+export -f git_pull
 
-if [[ -f "$2" ]]; then
+collect_clone() {
+  local file="$1"
+  local tmp
+  local result=""
+
   while read -r repo; do
 
     # ignore blank lines
     if [ "$repo" != "" ]; then
 
-      # save command output
-      out=$(git clone "$repo" 2>&1)
-
-      # if clone was unsuccessful try to enter folder and pull
-      if [[ $out =~ fatal ]]; then
-        dest=$(echo "$out" | awk '{print substr($4, 2, length($4) - 2)}') 
-        # echo $dest
-        cd "$1"/"$dest" || exit
-        echo -e "\033[0;33mupdating $dest:\033[0m"
-        git pull
-        cd .. || exit
-      else
-        echo "$out"
-      fi
+      tmp=$(sem -j 4 git_clone "$repo" 2>&1)
+      result+="${tmp},"
     fi
 
-  done <"$2"
-else
-  error_msg
-fi
+  done <"$file"
+  echo "$result"
 }
 
-git_clone "${@}"
+update_repos() {
+  local repos="$1"
+  IFS=,
+  for out in $repos; do
+    if [[ $out =~ fatal ]]; then
+      dest=$(echo "$out" | awk '{print substr($4, 2, length($4) - 2)}')
+      sem -j 4 git_pull "$dest"
+    else
+      echo "$out"
+    fi
+  done
+  sem --wait
+}
+
+ # echo -e "\033[0;33mupdating $dest...\033[0m"
+# take two args. Parent folder and repositories text file.
+# loop over each repo in text file, attempting to clone
+main () {
+  (( $# == 2 )) || error_msg
+
+  local out
+  local path="$1"
+  local repo_file="$2"
+  local clone_status
+  local dest
+
+  cd "$path" || exit
+
+  if [[ -f "$2" ]]; then
+    clone_status=$(collect_clone "$repo_file")
+    update_repos "$clone_status"
+  else
+    error_msg
+  fi
+}
+
+main "${@}"
 # git_pull
