49a6b65bc76045aba041b457f62fb47449bfea66
Daniel Berg <mail@roosta.sh>
2022-05-20 08:57:58 +0200

    screenlayout: fix lintilla

--- local/lintilla/conf/screenlayout/.screenlayout/main.sh
+++ local/lintilla/conf/screenlayout/.screenlayout/main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 5760x0 --rotate normal --output DP-1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DVI-D-1 --off
+xrandr --output DVI-I-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 5760x0 --rotate normal --output DP-1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal
