c9f2c47e874ba8cc8cc20818940a30765b0c8767
Daniel Berg <mail@roosta.sh>
2024-01-15 15:27:01 +0100

    vim/chatgpt: increase max_tokens

--- conf/vim/.vim/plugin/chatgpt.lua
+++ conf/vim/.vim/plugin/chatgpt.lua
@@ -149,7 +149,7 @@ require("chatgpt").setup({
     model = "gpt-3.5-turbo",
     frequency_penalty = 0,
     presence_penalty = 0,
-    max_tokens = 300,
+    max_tokens = 800,
     temperature = 0,
     top_p = 1,
     n = 1,
