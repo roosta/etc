bff5533e34b47564f61fcc28ee68234d20d7dc1c
Daniel Berg <mail@roosta.sh>
2024-07-19 18:45:08 +0200

    local: transfer lintilla vars + conf
    lintilla is no more, garkbit rises

--- local/garkbit/conf/screenlayout/.screenlayout/main.sh
+++ local/garkbit/conf/screenlayout/.screenlayout/main.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output DP1 --mode 3840x2160 --pos 1920x0 --rotate normal --output HDMI1 --off --output HDMI2 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI3 --off --output VGA1 --off --output VIRTUAL1 --off
+xrandr \
+  --output DisplayPort-0 --rate 60 --primary --mode 3840x2160 --pos 3840x1080 --rotate normal  \
+  --output DisplayPort-1 --mode 3840x2160 --pos 7680x1080 --rotate normal \
+  --output DisplayPort-2 --mode 3840x2160 --pos 0x1080 --rotate normal \
+  --output HDMI-A-0 --mode 1920x1080 --pos 4800x0 --rotate normal
