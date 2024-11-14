e3f2a14ce51b379b4d95d09809a5039c6e27e79e
Daniel Berg <mail@roosta.sh>
2024-11-14 14:59:36 +0100

    local/garkbit: set dpi in screen layout script
    Not that it did any good, but might as well keep it. Trying to fix small
    text issues with gtk

--- local/garkbit/conf/screenlayout/.screenlayout/main.sh
+++ local/garkbit/conf/screenlayout/.screenlayout/main.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr \
+xrandr --dpi 192 \
   --output DisplayPort-0 --mode 3840x2160 --pos 7680x1080 --rotate normal \
   --output DisplayPort-1 --mode 3840x2160 --pos 0x1080 --rotate normal \
   --output DisplayPort-2 --primary --mode 3840x2160 --pos 3840x1080 --rotate normal \
