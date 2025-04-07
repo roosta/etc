5aff8af1f11f178469beb47402d1de8531b31136
Daniel Berg <mail@roosta.sh>
2025-04-07 15:29:04 +0200

    vim/telescope: enable searching hidden files

--- conf/nvim/.config/nvim/lua/plugins/telescope.nvim.lua
+++ conf/nvim/.config/nvim/lua/plugins/telescope.nvim.lua
@@ -7,7 +7,28 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
+
     local builtin = require("telescope.builtin")
+
+    -- Hidden files
+    local telescope = require("telescope")
+    local telescopeConfig = require("telescope.config")
+
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+    table.insert(vimgrep_arguments, "--hidden")
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
+
+    telescope.setup({
+      defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+      },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+        }
+      }
+    })
     -- git
     vim.keymap.set("n", "<leader>gC", builtin.git_commits, { desc = "Telescope project commits", silent = true })
     vim.keymap.set("n", "<leader>gc", builtin.git_bcommits, { desc = "Telescope buffer commits", silent = true })
