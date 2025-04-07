76222a6459aaec1c3042482c2c91e7db0ceb82b8
Daniel Berg <mail@roosta.sh>
2025-04-07 23:29:54 +0200

    vim/render-markdown: config  comments

--- conf/nvim/.config/nvim/lua/plugins/render-markdown.nvim.lua
+++ conf/nvim/.config/nvim/lua/plugins/render-markdown.nvim.lua
@@ -2,13 +2,15 @@
 -- │┬┘├─ ││││ │├─ │┬┘──││││─┤│┬┘├┴┐│ ││ │││││││ ││││┌┘││││
 -- ┆└┘┴─┘┆└┘┆─┘┴─┘┆└┘  ┘ ┆┘ ┆┆└┘┆ ┘┆─┘┘─┘└┴┆┆└┘o┆└┘└┘ ┆┘ ┆
 -- ─────────────────────────────────────────────────────────────────────────
--- Plugin to improve viewing Markdown files in Neovim 
+-- Plugin to improve viewing Markdown files in Neovim
 
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   ft = { "markdown", "Avante" },
   opts = {
     file_types = { "markdown", "Avante" },
+    -- heading = { border = true },
+    -- anti_conceal = { enabled = false },
   },
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 }
