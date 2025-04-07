e5f77977cef1d5bbe5476accb40332aa057ae94e
Daniel Berg <mail@roosta.sh>
2025-04-07 16:14:01 +0200

    vim/plugins: add vim-hypr-nav

---
+++ conf/nvim/.config/nvim/lua/plugins/vim-hypr-nav.lua
@@ -0,0 +1,10 @@
+-- ┐ ┬o┌┌┐  ┬ ┬┐ ┬┬─┐┬─┐  ┌┐┐┬─┐┐ ┬
+-- │┌┘││││──│─┤└┌┘│─┘│┬┘──││││─┤│┌┘
+-- └┘ ┆┘ ┆  ┆ ┴ ┆ ┆  ┆└┘  ┆└┘┘ ┆└┘
+-- ─────────────────────────────────────────────────────────────────────────
+-- Seamless navigation between hyprland windows and (Neo)Vim splits with the
+-- same key bindings.
+
+return {
+  "nuchs/vim-hypr-nav",
+}
