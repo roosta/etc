508c94c0388196587b5da82c240546488fca5c95
Daniel Berg <mail@roosta.sh>
2025-04-09 01:14:33 +0200

    vim/keymaps: add treesitter toggle

--- conf/nvim/.config/nvim/lua/config/keymaps.lua
+++ conf/nvim/.config/nvim/lua/config/keymaps.lua
@@ -71,4 +71,7 @@ vim.keymap.set({ 'n', 'i', 'v' }, '<F1>', '<Nop>', {
   silent = true,
 })
 
+-- toggle treesitter
+vim.keymap.set('n', '<leader>ts', ':TSToggle highlight<cr>', { noremap = true, silent = true, desc = "Toggle Treesitter" })
+
 --  vim: set ts=2 sw=2 tw=0 fdm=marker et :
