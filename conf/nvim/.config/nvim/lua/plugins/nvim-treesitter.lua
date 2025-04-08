0800ff725f715e5a9839884891d9849c4d622a1f
Daniel Berg <mail@roosta.sh>
2025-04-08 15:20:34 +0200

    vim/treesitter: enable highlighting by default

--- conf/nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ conf/nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -6,7 +6,17 @@
 
 return {
   "nvim-treesitter/nvim-treesitter",
+  event = "VeryLazy",
   build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = { "vue", "javascript", "rust", "lua", "vim", "json", "toml" },
+      ignore_install = {}, -- List of parsers to ignore installing
+      highlight = {
+        enable = true
+      },
+    }
+  end
 }
 
 
