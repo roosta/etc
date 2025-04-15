32d3eb1c0b8b61d53edf6d5334de59235f86ef9b
Daniel Berg <mail@roosta.sh>
2025-04-15 17:24:43 +0200

    vim/telescope: apply formatting (80 columns)

--- conf/nvim/.config/nvim/lua/plugins/telescope.nvim.lua
+++ conf/nvim/.config/nvim/lua/plugins/telescope.nvim.lua
@@ -31,24 +31,67 @@ return {
     })
 
     -- git
-    vim.keymap.set("n", "<leader>gC", builtin.git_commits, { desc = "Telescope project commits", silent = true })
-    vim.keymap.set("n", "<leader>gc", builtin.git_bcommits, { desc = "Telescope buffer commits", silent = true })
+    vim.keymap.set("n", "<leader>gC", builtin.git_commits, {
+      desc = "Telescope project commits",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>gc", builtin.git_bcommits, {
+      desc = "Telescope buffer commits",
+      silent = true,
+    })
 
     -- Search
-    vim.keymap.set("n", "<leader>e", builtin.find_files, { desc = "Telescope find files", silent = true })
-    vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep", silent = true })
-    vim.keymap.set("n", "<leader>ss", builtin.current_buffer_fuzzy_find, { desc = "Telescope current buffer find", silent = true })
+    vim.keymap.set("n", "<leader>e", builtin.find_files, {
+      desc = "Telescope find files",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>/", builtin.live_grep, {
+      desc = "Telescope live grep",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>ss", builtin.current_buffer_fuzzy_find, {
+      desc = "Telescope current buffer find",
+      silent = true,
+    })
 
     -- jump
-    vim.keymap.set("n", "<leader>jI", builtin.lsp_workspace_symbols, { desc = "Telescope project symbols", silent = true })
-    vim.keymap.set("n", "<leader>ji", builtin.lsp_document_symbols, { desc = "Telescope buffer symbols", silent = true })
-    vim.keymap.set("n", "<leader>jj", builtin.treesitter, { desc = "Telescope treesitter", silent = true })
-    vim.keymap.set("n", "<leader>mm", builtin.marks, { desc = "Telescope marks", silent = true })
-    vim.keymap.set("n", "<leader>:", builtin.commands, { desc = "Telescope commands", silent = true })
-    vim.keymap.set("n", "<leader>bb", builtin.buffers, { desc = "Telescope buffers", silent = true })
-    vim.keymap.set("n", "<leader>H", builtin.help_tags, { desc = "Telescope help tags", silent = true })
-    vim.keymap.set("n", "<leader>?", builtin.keymaps, { desc = "Telescope normal keymappings", silent = true })
-    vim.keymap.set("n", "z=", builtin.spell_suggest, { noremap = true, desc = "Telescope spell suggestions", silent = true })
+    vim.keymap.set("n", "<leader>jI", builtin.lsp_workspace_symbols, {
+      desc = "Telescope project symbols",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>ji", builtin.lsp_document_symbols, {
+      desc = "Telescope buffer symbols",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>jj", builtin.treesitter, {
+      desc = "Telescope treesitter",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>mm", builtin.marks, {
+      desc = "Telescope marks",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>:", builtin.commands, {
+      desc = "Telescope commands",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>bb", builtin.buffers, {
+      desc = "Telescope buffers",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>H", builtin.help_tags, {
+      desc = "Telescope help tags",
+      silent = true,
+    })
+    vim.keymap.set("n", "<leader>?", builtin.keymaps, {
+      desc = "Telescope normal keymappings",
+      silent = true,
+    })
+    vim.keymap.set("n", "z=", builtin.spell_suggest, {
+      noremap = true,
+      desc = "Telescope spell suggestions",
+      silent = true,
+    })
 
     -- Find note files in $HOME/notes
     vim.keymap.set("n", "<leader>o", function()
